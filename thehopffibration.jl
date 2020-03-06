c863af757224d21c290553b9044f2b71d866aab3
Mohammad Javad Azadi <mjazadi42@gmail.com>
2020-03-06 17:32:08 +0330

    Added docstrings and reformatted the code.

--- thehopffibration.jl
+++ thehopffibration.jl
@@ -9,6 +9,13 @@ using ReferenceFrameRotations
 N = 30
 lspace = range(0.0, stop = 2pi, length = N)
 
+"""
+locate(θ, ψ)
+
+Locates the base point on the two sphere
+with the given latitude (θ) and longitude(ψ)
+both of which are measured in radians.
+"""
 function locate(θ, ψ)
     y₁ = cos(θ)*cos(ψ)
     y₂ = cos(θ)*sin(ψ)
@@ -16,6 +23,12 @@ function locate(θ, ψ)
     [y₁,y₂,y₃]
 end
 
+"""
+σ⁻¹(y₁, y₂, y₃)
+
+Sends a point on the plane back to a point on the unit sphere.
+It is the inverse stereographic projection of a three sphere.
+"""
 function σ⁻¹(y₁, y₂, y₃)
     x₁ = 2y₁ / (1 + y₁^2 + y₂^2 + y₃^2)
     x₂ = 2y₂ / (1 + y₁^2 + y₂^2 + y₃^2)
@@ -24,6 +37,12 @@ function σ⁻¹(y₁, y₂, y₃)
     [x₁, x₂, x₃, x₄]
 end
 
+"""
+σ(x₁, x₂, x₃, x₄)
+
+Sends a point on the unit sphere to a point on the plane.
+It is the stereographic projection of a three sphere.
+"""
 function σ(x₁, x₂, x₃, x₄)
     y₁ = x₁ / (1 - x₄)
     y₂ = x₂ / (1 - x₄)
@@ -31,42 +50,65 @@ function σ(x₁, x₂, x₃, x₄)
     [y₁, y₂, y₃]
 end
 
-function getpoints(y₁, y₂, y₃)
+"""
+points!(p)
+
+Finds 2 points on the fiber circle under stereographic projection 
+with the given point in the base space.
+"""
+function points!(p)
     λ₁ = complex(cos(1), sin(1))
     λ₂ = complex(cos(2), sin(2))
-    x₁, x₂, x₃, x₄ = σ⁻¹(y₁, y₂, y₃)
-    p₁ = λ₁ * complex(x₁, x₂)
-    p₂ = λ₁ * complex(x₃, x₄)
-    q₁ = λ₂ * complex(x₁, x₂)
-    q₂ = λ₂ * complex(x₃, x₄)
-    p = σ(real(p₁), imag(p₁), real(p₂), imag(p₂))
-    q = σ(real(q₁), imag(q₁), real(q₂), imag(q₂))
-    [p, q]
+    x₁, x₂, x₃, x₄ = σ⁻¹(p[1], p[2], p[3])
+    z₁ = λ₁ * complex(x₁, x₂)
+    z₂ = λ₁ * complex(x₃, x₄)
+    w₁ = λ₂ * complex(x₁, x₂)
+    w₂ = λ₂ * complex(x₃, x₄)
+    q = σ(real(z₁), imag(z₁), real(z₂), imag(z₂))
+    r = σ(real(w₁), imag(w₁), real(w₂), imag(w₂))
+    [q, r]
 end
 
-function getcenter(A, B, C)
+"""
+center!(A, B, C)
+
+Finds the center point of the fiber circle under stereographic projection
+with the given 3 points on the circle circumference.
+"""
+function center!(A, B, C)
     a = norm(B - C)
     b = norm(A - C)
     c = norm(A - B)
-    numerator = a^2 * (b^2 + c^2 - a^2) * A + b^2 * (a^2 + c^2 - b^2) * B + c^2 * (a^2 + b^2 + - c^2) * C
-    denominator = a^2 * (b^2 + c^2 - a^2) + b^2 * (a^2 + c^2 - b^2) + c^2 * (a^2 + b^2 - c^2)
+    numerator = a^2 * (b^2 + c^2 - a^2) * A + 
+                b^2 * (a^2 + c^2 - b^2) * B + 
+                c^2 * (a^2 + b^2 + - c^2) * C
+    denominator = a^2 * (b^2 + c^2 - a^2) + 
+                  b^2 * (a^2 + c^2 - b^2) + 
+                  c^2 * (a^2 + b^2 - c^2)
     numerator / denominator
 end
 
-function getcircle(location)
+"""
+fiber!(θ, ψ)
+
+Calculates the grid of a fiber circle under sterographic projection
+with the given coordinates in the base space: latitude (θ) and longitude(ψ),
+both of which are measured in radians.
+"""
+function fiber!(θ, ψ)
     # Locate the point on the two sphere
-    C = locate(location[1], location[2])
+    C = locate(θ, ψ)
     # Find two other points on the circle
-    A, B = getpoints(C[1],C[2],C[3])
+    A, B = points!(C)
     # Get the circle center point
-    Q = getcenter(A, B, C)
+    Q = center!(A, B, C)
     # Find the small and big radii
     r = 0.025
     R = Float64(norm(A - Q))
     # Construct a torus of revolution
-    x = Q[1] .+ [(R + r * cos(θ)) * cos(ϕ) for θ in lspace, ϕ in lspace]
-    y = Q[2] .+ [(R + r * cos(θ)) * sin(ϕ) for θ in lspace, ϕ in lspace]
-    z = Q[3] .+ [r * sin(θ) for θ in lspace, ϕ in lspace]
+    x = Q[1] .+ [(R + r * cos(i)) * cos(j) for i in lspace, j in lspace]
+    y = Q[2] .+ [(R + r * cos(i)) * sin(j) for i in lspace, j in lspace]
+    z = Q[3] .+ [r * sin(i) for i in lspace, j in lspace]
     points = [Point3f0(x[i], y[i], z[i]) for i in 1:length(x)]
     # Get the normal to the plane containing the points
     n = cross(A - Q, B - Q)
@@ -77,11 +119,14 @@ function getcircle(location)
     u = cross(n, i)
     u = u / norm(u)
     # The angle of rotation
-    θ = acos(dot(n, i)) / 2.0
-    q = ReferenceFrameRotations.Quaternion(cos(θ), sin(θ)*u[1], sin(θ)*u[2], sin(θ)*u[3])
-    rotatedpoints = [ReferenceFrameRotations.vect(q\[points[i][1]; 
-                                                     points[i][2]; 
-                                                     points[i][3]]*q) for i in 1:length(points)]
+      ϕ = acos(dot(n, i)) / 2.0
+    q = ReferenceFrameRotations.Quaternion(cos(ϕ), 
+                                           sin(ϕ)*u[1],
+                                           sin(ϕ)*u[2],
+                                           sin(ϕ)*u[3])
+    rotatedpoints = [vect(q\[points[i][1]; 
+                             points[i][2]; 
+                             points[i][3]]*q) for i in 1:length(points)]
     rotatedx = [rotatedpoints[i][1] for i in 1:length(rotatedpoints)]
     rotatedy = [rotatedpoints[i][2] for i in 1:length(rotatedpoints)]
     rotatedz = [rotatedpoints[i][3] for i in 1:length(rotatedpoints)]
@@ -91,67 +136,80 @@ function getcircle(location)
     [rotatedx, rotatedy, rotatedz]
 end
 
-function getmarker(location)
+"""
+base!(θ, ψ)
+
+Calculates the grid of a marker on the base two sphere
+with the given coordinates in the base space: latitude (θ) and longitude(ψ),
+both of which are measured in radians.
+"""
+function base!(θ, ψ)
     # Locate the point on the two sphere
-    l = locate(location[1], location[2])
+    l = locate(θ, ψ)
     # Construct a two sphere
     r = 0.05
-    x = l[1] .+ [r * cos(θ)*cos(ϕ) for θ in lspace, ϕ in lspace]
-    y = l[2] .+ [r * cos(θ)*sin(ϕ) for θ in lspace, ϕ in lspace]
-    z = l[3] .+ [r * sin(θ) for θ in lspace, ϕ in lspace]
+    x = l[1] .+ [r * cos(i)*cos(j) for i in lspace, j in lspace]
+    y = l[2] .+ [r * cos(i)*sin(j) for i in lspace, j in lspace]
+    z = l[3] .+ [r * sin(i) for i in lspace, j in lspace]
     [x, y, z]
 end
 
-function animate(location, i)
-    θ = 0.5659736245
-    ψ = 0.937032369
-    location[] = locate(θ, ψ + (i/100.0) * 2pi)
+"""
+animate(location, θ, ψ, sweep)
+
+Moves a point on a path parallel to the equator on the base space
+with the given location Node which is an observable, 
+the coordinates in the base space: latitude (θ) and longitude(ψ) in radians,
+and the sweep angle which ranges from 0 to 2pi.
+"""
+function animate(location, θ, ψ, sweep)
+    location[] = [θ, ψ + sweep]
 end
 
 sθ, oθ = textslider(-pi/2:0.01:pi/2, "θ", raw = true, camera = campixel!, start = 0)
 sψ, oψ = textslider(0:0.01:2pi, "ψ", raw = true, camera = campixel!, start = 0)
 
-location = @lift(locate($oθ, $oψ))
-
-circle = @lift(getcircle([$oθ $oψ]))
-
-marker = @lift(getmarker([$oθ $oψ]))
-
-color = @lift([RGBAf0($location[1], $location[2], $location[3]) for i in lspace, j in lspace])
+a = @lift([$oθ, $oψ])
 
 scene = Scene(show_axis = false)
 
 # The 3D coordinate indicator
-originc = Vec3f0(0); baselen = 0.05f0; dirlen = 0.5f0
+origin = Vec3f0(0); baselen = 0.05f0; dirlen = 0.5f0
 # create an array of differently colored boxes in the direction of the 3 axes
 rectangles = [
-    (HyperRectangle(Vec3f0(originc), Vec3f0(dirlen, baselen, baselen)), RGBAf0(0.5,1.0,0.5,0.9)),
-    (HyperRectangle(Vec3f0(originc), Vec3f0(baselen, dirlen, baselen)), RGBAf0(1.0,0.5,0.5,0.9)),
-    (HyperRectangle(Vec3f0(originc), Vec3f0(baselen, baselen, dirlen)), RGBAf0(0.5,0.5,1.0,0.9))
+    (HyperRectangle(Vec3f0(origin), Vec3f0(dirlen, baselen, baselen)), RGBAf0(0.5,1.0,0.5,0.9)),
+    (HyperRectangle(Vec3f0(origin), Vec3f0(baselen, dirlen, baselen)), RGBAf0(1.0,0.5,0.5,0.9)),
+    (HyperRectangle(Vec3f0(origin), Vec3f0(baselen, baselen, dirlen)), RGBAf0(0.5,0.5,1.0,0.9))
 ]
 meshes = map(GLNormalMesh, rectangles)
 mesh!(scene, merge(meshes), transparency = true)
 m = GLNormalUVMesh(Sphere(Point3f0(0), 1f0), 60)
-twosphere = mesh!(scene, m, color = RGBAf0(0.75,0.75,0.75,0.5), shading = false, transparency = true)
-circlesurface = surface!(scene, 
-                         @lift($circle[1]), 
-                         @lift($circle[2]), 
-                         @lift($circle[3]), 
-                         color = color, 
-                         shading = false)
-markersurface = surface!(scene, 
-                         @lift($marker[1]), 
-                         @lift($marker[2]), 
-                         @lift($marker[3]), 
-                         color = color, 
-                         shading = false)
+sphere = mesh!(scene, m, color = RGBAf0(0.75,0.75,0.75,0.5), shading = false, transparency = true)
+fiber = surface!(scene, 
+                 @lift(fiber!($a[1], $a[2])[1]), 
+                 @lift(fiber!($a[1], $a[2])[2]), 
+                 @lift(fiber!($a[1], $a[2])[3]), 
+                 color = @lift([RGBAf0(locate($a[1], $a[2])[1], 
+                                       locate($a[1], $a[2])[2], 
+                                       locate($a[1], $a[2])[3]) for i in lspace, j in lspace]), 
+                 shading = false)
+base = surface!(scene, 
+                @lift(base!($a[1], $a[2])[1]), 
+                @lift(base!($a[1], $a[2])[2]), 
+                @lift(base!($a[1], $a[2])[3]), 
+                color = @lift([RGBAf0(locate($a[1], $a[2])[1], 
+                                      locate($a[1], $a[2])[2], 
+                                      locate($a[1], $a[2])[3]) for i in lspace, j in lspace]), 
+                shading = false)
 fullscene = hbox(scene, vbox(sθ, sψ), parent = Scene(resolution = (500, 500)))
 
-#record(scene, "thehopffibration.gif") do io
-#    for i = 1:100
-#        animate(location, i) # animate scene
-#        recordframe!(io) # record a new frame
-#    end
-#end
+record(scene, "output.gif") do io
+    for sweep in range(0, stop = 2pi, length = 50)
+        θ = 0.5659736245
+        ψ = 0.937032369
+        animate(a, θ, ψ, sweep) # animate scene
+        recordframe!(io) # record a new frame
+    end
+end
 
 
