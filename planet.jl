544baebbdbf1216c67509ceaa7651d5511a5e280
Mohammad Javad Azadi <mjazadi42@gmail.com>
2020-03-19 09:35:46 +0330

    Added docstrings and reformatted the code.

--- planet.jl
+++ planet.jl
@@ -23,8 +23,14 @@ function sample(dataframe, max)
     sampled_latitudes = Array{Float64}(undef, max)
     count = length(total_longitudes)
     if count > max
-        sample!(total_longitudes, sampled_longitudes, replace=false, ordered=true)
-        sample!(total_latitudes, sampled_latitudes, replace=false, ordered=true)
+        sample!(total_longitudes,
+                sampled_longitudes,
+                replace=false,
+                ordered=true)
+        sample!(total_latitudes,
+                sampled_latitudes,
+                replace=false,
+                ordered=true)
         longitudes = sampled_longitudes
         latitudes = sampled_latitudes
     else
@@ -40,6 +46,12 @@ function sample(dataframe, max)
 end
 
 
+"""
+π(z, w)
+
+Sends a point on a unit 3-sphere to a point on a unit 2-sphere with the given
+complex numbers representing a unit quaternion. S³ ↦ S² (x,y,z)
+"""
 function π(z, w)
     x₁ = real(z)
     x₂ = imag(z)
@@ -49,27 +61,61 @@ function π(z, w)
 end
 
 
+"""
+σ(ϕ, θ; β=0)
+
+Sends a point on a 2-sphere to a point on a 3-sphere with the given longitude
+and latitude coordinate in radians. S² ↦ S³
+"""
 function σ(ϕ, θ; β=0)
     exp(-im * (β + ϕ)) * sqrt((1 + sin(θ)) / 2), sqrt((1 - sin(θ)) / 2)
 end
 
 
+"""
+τ(ϕ, θ; β=0)
+
+Sends a point on a 2-sphere to a point on a 3-sphere with the given longitude
+and latitude coordinate in radians. S² ↦ S³
+"""
 function τ(ϕ, θ; β=0)
     sqrt((1 + sin(θ)) / 2), exp(im * (β + ϕ)) * sqrt((1 - sin(θ)) / 2)
 end
 
 
+"""
+S¹action(α, z, w)
+
+Performs a group action corresponding to moving along the circumference of a
+circle with the given angle and the complex numbers representing a unit
+quaternion on a 3-sphere.
+"""
 function S¹action(α, z, w)
     exp(im * α) * z, exp(im * α) * w
 end
 
 
+"""
+λ(z, w)
+
+Sends a point on a 3-sphere to a point in the plane x₄=0 with the given complex
+numbers representing a unit quaternion. This is the stereographic projection.
+S³ ↦ R³
+"""
 function λ(z, w)
     [real(z), imag(z), real(w)] ./ (1 - imag(w))
 end
 
 
-function get_manifold(points, segments, cut)
+"""
+get_manifold(points, segments, distance, cut)
+
+Calculates a grid of points in R³ for constructing a surface in a specific way
+with the given points in the base space (a 2-sphere,) the number of cross
+sections, the distance from the z axis and the angle that determines how much
+of the grid is the leftover after the cut.
+"""
+function get_manifold(points, segments, distance, cut)
     samples = size(points, 1)
     leftover_segments = Integer(floor((cut / 2pi) * segments))
     manifold_segments = segments - leftover_segments
@@ -82,8 +128,8 @@ function get_manifold(points, segments, cut)
         z, w = σ(ϕ, -θ)
         for j in 1:segments
             if j ≤ manifold_segments
-                x₁ = (real(z) + 2) * sin(α*(j-1))
-                x₂ = (real(z) + 2) * cos(α*(j-1))
+                x₁ = (real(z) + distance) * sin(α*(j-1))
+                x₂ = (real(z) + distance) * cos(α*(j-1))
                 x₃ = imag(z)
                 manifold[j, i, :] = [x₁, x₂, x₃]
                 if j != manifold_segments
@@ -91,8 +137,8 @@ function get_manifold(points, segments, cut)
                 end
             else
                 index = j-manifold_segments
-                x₁ = (real(z) + 2) * sin((2pi-cut)+γ*(index-1))
-                x₂ = (real(z) + 2) * cos((2pi-cut)+γ*(index-1))
+                x₁ = (real(z) + distance) * sin((2pi-cut)+γ*(index-1))
+                x₂ = (real(z) + distance) * cos((2pi-cut)+γ*(index-1))
                 x₃ = imag(z)
                 leftover[index, i, :] = [x₁, x₂, x₃]
                 z, w = S¹action(γ, z, w)
@@ -102,11 +148,14 @@ function get_manifold(points, segments, cut)
     manifold, leftover
 end
 
-
+# The scene object that contains other visual objects
 universe = Scene(backgroundcolor = :black, show_axis=false)
+# Use a slider for rotating the base space in an interactive way
 sg, og = textslider(0:0.05:2pi, "g", start = 0)
 
+# The maximum number of points to sample from the dataset for each country
 max_samples = 300
+# The number of cross sections for constructing a manifold
 segments = 90
 # Made with Natural Earth.
 # Free vector and raster map data @ naturalearthdata.com.
@@ -117,8 +166,13 @@ countries = Dict("iran" => [1.0, 0.0, 0.0], # red
                  "australia" => [1.0, 0.804, 0.0], # orange
                  "germany" => [0.914, 0.0, 1.0], # purple
                  "israel" => [0.0, 1.0, 0.075]) # green
+# The path to the dataset
 path = "data/natural_earth_vector"
+# The angle to cut the manifolds for a better visualization
 cut = 2pi/360*80
+# The distance from the z axis
+distance = 2
+# Construct a manifold for each country in the dictionary
 for country in countries
     dataframe = CSV.read(joinpath(path, "$(country[1])-nodes.csv"))
     points = sample(dataframe, max_samples)
@@ -139,7 +193,7 @@ for country in countries
     leftover_segments = Integer(floor((cut / 2pi) * segments))
     manifold_segments = segments - leftover_segments
     manifold_color = fill(specific, manifold_segments, samples)
-    manifolds = @lift(get_manifold($rotated, segments, cut))
+    manifolds = @lift(get_manifold($rotated, segments, distance, cut))
     manifold = @lift($manifolds[1])
     surface!(universe,
              @lift($manifold[:, :, 1]),
@@ -158,17 +212,21 @@ for country in countries
      end
 end
 
-disk_segments = 10
-disk_samples = 30
-phase = -pi/2 + pi/100
-align = 0.35
+# Construct the 2 disks that show the base map
+disk_segments = 20
+disk_samples = 60
+# Parameters for aligning the base map and the fibers
+longitude_align = -pi/2
+latitude_align = 0.35
 lspace = range(0, stop = 2pi, length = disk_samples)
 disk1 = @lift begin
     p = Array{Float64}(undef, disk_segments, disk_samples, 3)
     for i in 1:disk_segments
         p[i, :, 1] = [0 for j in lspace]
-        p[i, :, 2] = [(i+align)/10*sin(j+$og+phase)+2 for j in lspace]
-        p[i, :, 3] = [(i+align)/10*cos(j+$og+phase) for j in lspace]
+        p[i, :, 2] = [(i+latitude_align)/disk_segments*
+                      sin(j+$og+longitude_align) + distance for j in lspace]
+        p[i, :, 3] = [(i+latitude_align)/disk_segments*
+                      cos(j+$og+longitude_align) for j in lspace]
     end
     p
 end
@@ -176,61 +234,54 @@ end
 disk2 = @lift begin
     p = Array{Float64}(undef, disk_segments, disk_samples, 3)
     for i in 1:disk_segments
-        p[i, :, 1] = [((i+align)/10*sin(j+$og+phase+cut)+2)*sin(2pi-cut)
+        p[i, :, 1] = [((i+latitude_align)/disk_segments*
+                       sin(j+$og+longitude_align+cut)+distance)*sin(2pi-cut)
                       for j in lspace]
-        p[i, :, 2] = [((i+align)/10*sin(j+$og+phase+cut)+2)*cos(2pi-cut)
+        p[i, :, 2] = [((i+latitude_align)/disk_segments*
+                       sin(j+$og+longitude_align+cut)+distance)*cos(2pi-cut)
                       for j in lspace]
-        p[i, :, 3] = [(i+align)/10*cos(j+$og+phase+cut) for j in lspace]
+        p[i, :, 3] = [(i+latitude_align)/disk_segments*
+                      cos(j+$og+longitude_align+cut) for j in lspace]
     end
     p
 end
 
-diskgrid_segments = 20
-diskgrid_samples = 60
-grid_lspace = range(0, stop = 2pi, length = diskgrid_samples)
-diskgrid1 = @lift begin
-    p = Array{Float64}(undef, diskgrid_segments, diskgrid_samples, 3)
-    for i in 1:diskgrid_segments
-        p[i, :, 1] = [0 for j in grid_lspace]
-        p[i, :, 2] = [2i/diskgrid_segments*sin(j+$og+phase)+2
-                      for j in grid_lspace]
-        p[i, :, 3] = [2i/diskgrid_segments*cos(j+$og+phase)
-                      for j in grid_lspace]
+# Construct the 2 disks that show the guidance grid
+grid1 = @lift begin
+    p = Array{Float64}(undef, disk_segments, disk_samples, 3)
+    for i in 1:disk_segments
+        p[i, :, 1] = [0 for j in lspace]
+        p[i, :, 2] = [2i/disk_segments*sin(j+$og+longitude_align)+distance
+                      for j in lspace]
+        p[i, :, 3] = [2i/disk_segments*cos(j+$og+longitude_align)
+                      for j in lspace]
     end
     p
 end
 
-diskgrid2 = @lift begin
-    p = Array{Float64}(undef, diskgrid_segments, diskgrid_samples, 3)
-    for i in 1:diskgrid_segments
-        p[i, :, 1] = [(2i/diskgrid_segments*sin(j+$og+phase+cut)+2)*sin(2pi-cut)
-                      for j in grid_lspace]
-        p[i, :, 2] = [(2i/diskgrid_segments*sin(j+$og+phase+cut)+2)*cos(2pi-cut)
-                      for j in grid_lspace]
-        p[i, :, 3] = [2i/diskgrid_segments*cos(j+$og+phase+cut)
-                      for j in grid_lspace]
+grid2 = @lift begin
+    p = Array{Float64}(undef, disk_segments, disk_samples, 3)
+    for i in 1:disk_segments
+        p[i, :, 1] = [(2i/disk_segments*
+                       sin(j+$og+longitude_align+cut)+distance)*sin(2pi-cut)
+                      for j in lspace]
+        p[i, :, 2] = [(2i/disk_segments*
+                       sin(j+$og+longitude_align+cut)+distance)*cos(2pi-cut)
+                      for j in lspace]
+        p[i, :, 3] = [2i/disk_segments*cos(j+$og+longitude_align+cut)
+                      for j in lspace]
     end
     p
 end
 
-image = try
-    load("data/BaseMap.png")
-catch e
-    @warn("Loading the globe map failed. Using random image, so this test will fail! (error: $e)")
-    rand(RGBAf0, 100, 100) # don't error test when e.g. offline
-end
-grid_image = try
-    load("data/boqugrid.png")
-catch e
-    @warn("Loading the globe map failed. Using random image, so this test will fail! (error: $e)")
-    rand(RGBAf0, 100, 100) # don't error test when e.g. offline
-end
+base_image = load("data/BaseMap.png")
+grid_image = load("data/boqugrid.png")
 
 surface!(universe,
          @lift($disk1[:, :, 1]),
          @lift($disk1[:, :, 2]),
          @lift($disk1[:, :, 3]),
-         color = image,
+         color = base_image,
          transparency = false,
          shading = false)
          
@@ -238,26 +289,27 @@ surface!(universe,
          @lift($disk2[:, :, 1]),
          @lift($disk2[:, :, 2]),
          @lift($disk2[:, :, 3]),
-         color = image,
+         color = base_image,
          transparency = false,
          shading = false)
 
 surface!(universe,
-         @lift($diskgrid1[:, :, 1]),
-         @lift($diskgrid1[:, :, 2]),
-         @lift($diskgrid1[:, :, 3]),
+         @lift($grid1[:, :, 1]),
+         @lift($grid1[:, :, 2]),
+         @lift($grid1[:, :, 3]),
          color = grid_image,
          transparency = false,
          shading = false)
 
 surface!(universe,
-         @lift($diskgrid2[:, :, 1]),
-         @lift($diskgrid2[:, :, 2]),
-         @lift($diskgrid2[:, :, 3]),
+         @lift($grid2[:, :, 1]),
+         @lift($grid2[:, :, 2]),
+         @lift($grid2[:, :, 3]),
          color = grid_image,
          transparency = false,
          shading = false)
 
+# Instantiate a horizontal box for holding the visuals and the controls
 scene = hbox(universe,
              vbox(sg),
              parent = Scene(resolution = (400, 400)))
@@ -267,6 +319,7 @@ eye_position, lookat, upvector = Vec3f0(-4, 4, 4), Vec3f0(0), Vec3f0(0, 0, 1.0)
 update_cam!(universe, eye_position, lookat)
 universe.center = false # prevent scene from recentering on display
 
+# Add stars to the scene to fill the background with something
 stars = 10_000
 scatter!(
     universe,
@@ -277,7 +330,6 @@ scatter!(
     show_axis = false, transparency = true
 )
 
-
 record(universe, "planet.gif") do io
     frames = 100
     for i in 1:frames
@@ -286,5 +338,4 @@ record(universe, "planet.gif") do io
         recordframe!(io) # record a new frame
     end
 end
-
 
