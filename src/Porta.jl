82e9e4ab06b0618016f7e262278bd404842c9994
Mohammad Javad Azadi <mjazadi42@gmail.com>
2020-06-24 00:30:37 +0430

    included the new source code and tests.

--- src/Porta.jl
+++ src/Porta.jl
@@ -3,9 +3,22 @@ __precompile__()
 
 module Porta
 
-include("basemap.jl")
-include("thehopffibration.jl")
-include("data.jl")
-include("signal.jl")
+dir = "./linearalgebra/"
+
+include(dir * "innerproductreal.jl")
+include(dir * "determinant.jl")
+include(dir * "outerproductreal.jl")
+include(dir * "real3.jl")
+include(dir * "innerproductreal3.jl")
+include(dir * "outerproductreal3.jl")
+include(dir * "quaternion.jl")
+include(dir * "innerproductquaternion.jl")
+include(dir * "abstractalgebrautils.jl")
+include("rotations.jl")
+include("body.jl")
+#include("basemap.jl")
+#include("thehopffibration.jl")
+#include("data.jl")
+#include("signal.jl")
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,29 @@
 using Test
+using Porta
+
+
+d = Int(floor(rand() * 5)) # dimension
+dir = "./linearalgebra_tests/"
 
 start = time()
-@time @testset "The Calculus Tests" begin include("calculus_tests.jl") end
-@time @testset "The Base Map Tests" begin include("basemap_tests.jl") end
-@time @testset "The Hopf Fibration Tests" begin include("thehopffibration_tests.jl") end
+@time @testset "The Inner Product Tests" begin
+    include(dir * "innerproductreal_tests.jl") end
+@time @testset "The Determinant Tests" begin include(dir * "determinant_tests.jl") end
+@time @testset "The Outer Product Tests" begin
+    include(dir * "outerproductreal_tests.jl") end
+@time @testset "The ℝ³ Tests" begin include(dir * "real3_tests.jl") end
+@time @testset "The Inner Product ℝ³ Tests" begin
+    include(dir * "innerproductreal3_tests.jl") end
+@time @testset "The Outer Product ℝ³ Tests" begin
+    include(dir * "outerproductreal3_tests.jl") end
+@time @testset "The Quaternion Tests" begin include(dir * "quaternion_tests.jl") end
+@time @testset "The Inner Product Quaternion Tests" begin
+    include(dir * "innerproductquaternion_tests.jl") end
+@time @testset "The Abstract Algebra Utils Tests" begin
+    include(dir * "abstractalgebrautils_tests.jl") end
+@time @testset "The Rotations Tests" begin include("rotations_tests.jl") end
+#@time @testset "The Calculus Tests" begin include("calculus_tests.jl") end
+#@time @testset "The Base Map Tests" begin include("basemap_tests.jl") end
+#@time @testset "The Hopf Fibration Tests" begin include("thehopffibration_tests.jl") end
 elapsed = time() - start
 println("Testing took", elapsed)
