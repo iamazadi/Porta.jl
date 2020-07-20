d0fbb0ad6e5fe237ef5368789b30e885e545bd74
Mohammad Javad Azadi <mjazadi42@gmail.com>
2020-07-20 22:09:58 +0430

    Used map function instead of list comprehension.

--- src/linearalgebra/outerproductreal.jl
+++ src/linearalgebra/outerproductreal.jl
@@ -5,5 +5,5 @@ export cross
 outer(a::Vector{Float64}, b::Vector{Float64}) = a * transpose(b)
 cross(a::Vector{Float64}, b::Vector{Float64}) = begin
     M = convert(Array{Float64}, transpose(reshape([a; a; b], :, length(a))))
-    [cofactor(M, 1, i) for i in 1:length(a)]
+    map(x -> cofactor(M, 1, x), 1:length(a))
 end
