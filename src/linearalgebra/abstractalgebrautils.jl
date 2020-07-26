a6695249f4049c37fe3f78dfe0dcb95f44870185
Mohammad Javad Azadi <mjazadi42@gmail.com>
2020-07-27 02:47:48 +0430

    Removed unnecessary code.

--- src/linearalgebra/abstractalgebrautils.jl
+++
@@ -1,6 +0,0 @@
-export normalize
-
-
-normalize(v::Vector{Float64}) = v ./ norm(v)
-normalize(r::ℝ³) = 1 / norm(r) * r
-normalize(h::ℍ) = 1 / norm(h) * h
--- test/linearalgebra_tests/abstractalgebrautils_tests.jl
+++
@@ -1,6 +0,0 @@
-r = ℝ³(rand(3))
-h = ℍ(rand(4))
-
-
-@test isapprox(norm(normalize(r)), 1)
-@test isapprox(norm(normalize(h)), 1)
