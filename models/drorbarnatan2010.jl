3b59b407afdbd49de6f91daea69b9c5ad5dc4776
Mohammad Javad Azadi <mjazadi42@gmail.com>
2020-07-28 09:41:50 +0430

    Used the same Quaternion for both S2 and S3 rotations.

--- models/drorbarnatan2010.jl
+++ models/drorbarnatan2010.jl
@@ -1,3 +1,4 @@
+import AbstractPlotting
 import Makie
 
 
@@ -24,7 +25,7 @@ Map from S² into the upper hemisphere of S² with the given point `p`.
 function f(p::Geographic)
     ϕ, θ = p.ϕ, p.θ
     r = sqrt((1 - sin(θ)) / 2)
-    r .* (cos(ϕ), sin(ϕ))
+    Geographic(r * cos(ϕ), r * sin(ϕ))
 end
 
 
@@ -70,12 +71,25 @@ end
 
 Build a surface with the given `scene`, `surface` and `color`.
 """
-function build(scene, surface, color)
+function build(scene, surface, color; transparency = false)
     x = Makie.Node(map(x -> vec(x)[1] , surface[:, :]))
     y = Makie.Node(map(x -> vec(x)[2] , surface[:, :]))
     z = Makie.Node(map(x -> vec(x)[3] , surface[:, :]))
-    Makie.surface!(scene, x, y, z, color = color)
+    Makie.surface!(scene, x, y, z, color = color, transparency = transparency)
     x, y, z
+end
+
+
+"""
+    update(o, p)
+
+Update an array of observables `o` with the given array of points `p`.
+"""
+function update(o, p::Array{ℝ³,2})
+    x, y, z = o
+    x[] = map(i -> vec(i)[1] , p[:, :])
+    y[] = map(i -> vec(i)[2] , p[:, :])
+    z[] = map(i -> vec(i)[3] , p[:, :])
 end
 
 
@@ -84,13 +98,11 @@ end
 
 Calculate the pullback to S³ with the given point `p` and the given angle `α`.
 """
-function pullback(p::Geographic, α::Real, angle::Real=0)
-    ϕ, θ = p.ϕ, p.θ
-    ϕ = ϕ - angle
-    r = sqrt((1 - sin(θ)) / 2)
-    ϕ, θ = r .* (cos(ϕ), sin(ϕ))
-    ϕ, θ = ϕ + pi, (θ + pi / 2) / 2
-    Quaternion(ComplexPlane(sin(θ) * exp(im * (ϕ + α) / 2), cos(θ) * exp(im * (α - ϕ) / 2)))
+function pullback(p::Geographic, α::Real)
+    p′ = f(p)
+    ϕ, θ = p′.ϕ + pi, (p′.θ + pi/2) / 2
+    Quaternion(ComplexPlane(sin(θ) * exp(im * (ϕ + α) / 2),
+                            cos(θ) * exp(im * (α - ϕ) / 2)))
 end
 
 
@@ -99,18 +111,17 @@ end
 
 
 """
-    getsurface(τ, q, p, s)
+    getsurface(q, p, s)
 
 Calculate a pullback surface using stereographic projection with the given S² rotation `q`,
 an array of points 'p' and the number of segments `s`.
 """
-function getsurface(τ::Real, q::Quaternion, p::Array{Geographic,1}, s::Int)
-    number = length(p)
-    surface = Array{ℝ³}(undef, s, number)
+function getsurface(q::Quaternion, p::Array{Geographic,1}, s::Int)
+    surface = Array{ℝ³}(undef, s, length(p))
     lspace = range(α₁, stop = α₂, length = s)
     for (i, α) in enumerate(lspace)
-        for j in 1:number
-            surface[i, j] = σmap(rotate(q, pullback(p[j], α, τ)))
+        for (j, point) in enumerate(p)
+            surface[i, j] = σmap(rotate(q, pullback(point, α)))
         end
     end
     surface
@@ -123,14 +134,13 @@ end
 Calculate a Riemann sphere with the given S² rotation `q` and circle `α`.
 """
 function sphere(q::Quaternion, α::Real, segments::Int=30)
-    longitudeoffset = 0.02 * pi
     latitudeoffset = -pi / 3
     s2 = Array{ℝ³}(undef, segments, segments)
     lspace = collect(range(-pi, stop = pi, length = segments)) #.+ longitudeoffset
     lspace2 = collect(range(pi / 2, stop = latitudeoffset, length = segments))
     for (i, θ) in enumerate(lspace2)
         for (j, ϕ) in enumerate(lspace)
-            h = pullback(Geographic(ϕ, θ), α, 0)
+            h = pullback(Geographic(ϕ, θ), α)
             s2[i, j] = σmap(rotate(q, h))
         end
     end
@@ -153,8 +163,11 @@ path = "test/data/natural_earth_vector"
 # The scene object that contains other visual objects
 scene = Makie.Scene(backgroundcolor = :white,
                     show_axis = false,
-                    resolution = (360, 360),
-                    camera = Makie.cam3d_cad!)
+                    resolution = (360, 360))
+#scene = Makie.Scene(backgroundcolor = :white,
+#                    show_axis = false,
+#                    resolution = (360, 360),
+#                    camera = Makie.cam3d_cad!)
 # Use a slider for rotating the base space in an interactive way
 #sg, og = textslider(0:0.05:2pi, "g", start = 0)
 # Instantiate a horizontal box for holding the visuals and the controls
@@ -167,15 +180,24 @@ maxsamples = 300
 segments = 30
 q = Quaternion(α₁, ℝ³(0, 0, 1))
 observables = []
+ghosts = []
+ghostpoints = []
 points = []
 for country in countries
-    dataframe = CSV.read(joinpath(path, "$(country[1])-nodes.csv"))
+    countryname = country[1]
+    dataframe = CSV.read(joinpath(path, "$(countryname)-nodes.csv"))
     # Sample a random subset of the points
     p = sample(dataframe, maxsamples)
     color = fill(Makie.RGBAf0(country[2]..., 1.0), segments, length(p))
-    x, y, z = build(scene, getsurface(0, q, p, segments), color)
+    x, y, z = build(scene, getsurface(q, p, segments), color)
     push!(observables, (x, y, z))
     push!(points, p)
+    if countryname in ["iran", "us", "china"]
+        color = fill(Makie.RGBAf0(country[2]..., 0.5), segments, length(p))
+        x, y, z = build(scene, getsurface(q, p, segments), color, transparency = true)
+        push!(ghosts, (x, y, z))
+        push!(ghostpoints, p)
+    end
 end
 
 
@@ -186,31 +208,26 @@ s2observables2 = build(scene, sphere(q, α₂, segments), s2color)
 frames = 90
 function animate(i)
     τ = i / frames * 2pi
+    global α₁ = 2(40 / 180 * pi) + 2τ
+    global α₂ = 2(2pi - 40 / 180 * pi) + 2τ
+    q = Quaternion(τ, ℝ³(0, 0, 1))
     for (p, nodes) in zip(points, observables)
-        x, y, z = nodes
-        surface = getsurface(τ, Quaternion(0, ℝ³(0, 0, 1)), p, segments)
-        x[] = map(i -> vec(i)[1] , surface[:, :])
-        y[] = map(i -> vec(i)[2] , surface[:, :])
-        z[] = map(i -> vec(i)[3] , surface[:, :])
+        update(nodes, getsurface(q, p, segments))
     end
-    x, y, z = s2observables
-    s2 = sphere(Quaternion(τ/2, ℝ³(0, 0, 1)), τ + α₁, segments)
-    x[] = map(i -> vec(i)[1] , s2[:, :])
-    y[] = map(i -> vec(i)[2] , s2[:, :])
-    z[] = map(i -> vec(i)[3] , s2[:, :])
-    x, y, z = s2observables2
-    s2 = sphere(Quaternion(τ/2, ℝ³(0, 0, 1)), τ + α₂, segments)
-    x[] = map(i -> vec(i)[1] , s2[:, :])
-    y[] = map(i -> vec(i)[2] , s2[:, :])
-    z[] = map(i -> vec(i)[3] , s2[:, :])
+    update(s2observables, sphere(q, α₁, segments))
+    update(s2observables2, sphere(q, α₂, segments))
+    global α₁ = 2(40 / 180 * pi) + 2τ
+    global α₂ = 2(-40 / 180 * pi) + 2τ
+    for (p, nodes) in zip(ghostpoints, ghosts)
+        update(nodes, getsurface(q, p, segments))
+    end
 end
-
 
 # update eye position
 # scene.camera.eyeposition.val
 upvector = Makie.Vec3f0(1, 0, 1)
-eyeposition = 0.8 .* Makie.Vec3f0(1, 3, 0)
-lookat = Makie.Vec3f0(0.5, 0, 0)
+eyeposition = Makie.Vec3f0(1/3, 1, 1/3) .* sqrt(3)
+lookat = Makie.Vec3f0(0, 0, 0)
 Makie.update_cam!(scene, eyeposition, lookat, upvector)
 scene.center = false # prevent scene from recentering on display
 
