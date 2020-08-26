73821650913fda8558cc5bf34f53354dcb6cf351
Mohammad Javad Azadi <mjazadi42@gmail.com>
2020-08-26 09:15:24 +0430

    Added an optional arg, TOLERANCE, to the generic function isapprox.

--- src/linearalgebra/abstractvectorspace.jl
+++ src/linearalgebra/abstractvectorspace.jl
@@ -24,7 +24,6 @@ abstract type VectorSpace end
 
 vec(vs::VectorSpace) = Base.vec(vs.a)
 show(io::IO, vs::VectorSpace) = Base.show(io, vec(vs))
-isapprox(vs1::VectorSpace, vs2::VectorSpace) = Base.isapprox(vec(vs1), vec(vs2))
 length(vs::VectorSpace) = Base.length(vec(vs))
 transpose(vs::VectorSpace) = Base.transpose(vec(vs))
 I(vs::VectorSpace) = reshape([Float64(i == j) for i in 1:length(vs) for j in 1:length(vs)],
@@ -36,3 +35,7 @@ dot(vs1::VectorSpace, vs2::VectorSpace) = dot(vs1, vs2, I(vs1))
 norm(vs::VectorSpace) = sqrt(dot(vs, vs))
 outer(vs1::VectorSpace, vs2::VectorSpace) = vec(vs1) * transpose(vs2)
 normalize(vs::VectorSpace) = vs * (1 / norm(vs))
+
+Base.isapprox(vs1::VectorSpace, vs2::VectorSpace; atol::Float64 = TOLERANCE) = begin
+    isapprox(vec(vs1), vec(vs2), atol = atol)
+end
