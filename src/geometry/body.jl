ac7c209b2e1da26a0ee5deac54ff03d82ae7f234
Mohammad Javad Azadi <mjazadi42@gmail.com>
2020-08-26 09:08:18 +0430

    Added functions to build a few common geometrical objects, triad, torus, sphere, etc.

--- src/geometry/body.jl
+++ src/geometry/body.jl
@@ -1,12 +1,106 @@
-export getsprite
+export constructtriad
+export constructtorus
+export constructsphere
+export constructcylinder
+export constructbox
 export getplane
-export getdisk
 
 
-getsprite(p::ℝ³, q::Quaternion; len=1.0) = begin
-    map(x -> x + p, rotate(ℝ³.([[0.0; 0.0; 0.0], [len; 0.0; 0.0],
-                                [0.0; 0.0; 0.0], [0.0; len; 0.0],
-                                [0.0; 0.0; 0.0], [0.0; 0.0; len]]), q))
+"""
+    constructtriad(q [; length])
+
+Construct a triad with the given configuration `q` and `length`.
+"""
+function constructtriad(q::Biquaternion; length::Float64 = 1.0)
+    map(x -> x + gettranslation(q), rotate(ℝ³.([[0; 0; 0], [length; 0; 0],
+                                                [0; 0; 0], [0; length; 0],
+                                                [0; 0; 0], [0; 0; length]]),
+                                           getrotation(q)))
+end
+
+
+"""
+    constructtorus(q, r, R)
+
+Construct a torus of revolution with the given configuration `q`, the smaller radius `r`
+and the bigger radius `R`.
+"""
+function constructtorus(q::Biquaternion,
+                        r::Real,
+                        R::Real;
+                        segments::Int = 36)
+    array = Array{ℝ³,2}(undef, segments, segments)
+    for i in 1:segments
+        for j in 1:segments
+            ϕ = i * 2pi / (segments - 1)
+            θ = j * 2pi / (segments - 1)
+            x₁ = (R + r * cos(ϕ)) * cos(θ)
+            x₂ = (R + r * cos(ϕ)) * sin(θ)
+            x₃ = r * sin(ϕ)
+            array[i, j] = ℝ³(x₁, x₂, x₃)
+        end
+    end
+    map(x -> x + gettranslation(q), rotate(array, getrotation(q)))
+end
+
+
+"""
+    constructsphere(q, radius)
+
+Construct a sphere with the given configuration `q` and `radius`.
+"""
+function constructsphere(q::Biquaternion,
+                         radius::Real;
+                         segments::Int = 36)
+    array = Array{ℝ³,2}(undef, segments, segments)
+    lspace = collect(range(float(-pi), stop = float(pi), length = segments))
+    for i in 1:segments
+        for j in 1:segments
+            ϕ = lspace[i]
+            θ = lspace[j] / 2
+            array[i, j] = ℝ³(Cartesian(Geographic(ϕ, θ))) * float(radius)
+        end
+    end
+    map(x -> x + gettranslation(q), rotate(array, getrotation(q)))
+end
+
+
+"""
+    constructcylinder(q, height, radius)
+
+Construct a cylinder with the given configuration `q`, `height` and `radius`.
+"""
+function constructcylinder(q::Biquaternion,
+                           height::Float64,
+                           radius::Float64;
+                           segments::Int = 36)
+    array = Array{ℝ³,2}(undef, segments, segments)
+    lspace = collect(range(float(-pi), stop = float(pi), length = segments))
+    for i in 1:segments
+        for j in 1:segments
+            α = lspace[j]
+            x₁ = radius * cos(α)
+            x₂ = radius * sin(α)
+            x₃ = (i - 1) * height / (segments - 1)
+            array[i, j] = ℝ³(x₁, x₂, x₃)
+        end
+    end
+    map(x -> x + gettranslation(q), rotate(array, getrotation(q)))
+end
+
+
+"""
+    constructbox(p, q, s)
+
+Construct a box with the given translation `p`, rotation `q` and scale `s`.
+"""
+function constructbox(p::ℝ³, q::Quaternion, s::ℝ³)
+    x, y, z = vec(s) ./ 2
+    points = [ℝ³(-x, -y, -z) ℝ³(x, -y, -z) ℝ³(-x, -y, -z) ℝ³(x, -y, -z);
+              ℝ³(-x, -y, +z) ℝ³(x, -y, +z) ℝ³(-x, -y, +z) ℝ³(x, -y, +z);
+              ℝ³(-x, +y, +z) ℝ³(x, +y, +z) ℝ³(-x, +y, +z) ℝ³(x, +y, +z);
+              ℝ³(-x, +y, -z) ℝ³(x, +y, -z) ℝ³(-x, +y, -z) ℝ³(x, +y, -z)]
+    map(x -> x + p, rotate(points, q))
 end
 
 
--- test/geometry_tests/body_tests.jl
+++ test/geometry_tests/body_tests.jl
@@ -1,11 +1,22 @@
+q = Biquaternion(Quaternion(0, ℝ³(0, 0, 1)), ℝ³(0, 0, 0))
+triad = constructtriad(q, length = 1.0)
+torus = constructtorus(q, 1, 10)
+sphere = constructsphere(q, 1)
+cylinder = constructcylinder(q, 1.0, 0.1)
+scale = ℝ³(1, 1, 1)
+box = constructbox(ℝ³(rand(3)), Quaternion(rand() * 2pi, ℝ³(rand(3))), scale)
 plane = getplane([0.0; 5.0; 3.5], Quaternion(1, 0, 0, 0), [1.0; 13.0; 13.0])
-sprite = getsprite(ℝ³(0, 0, 0), Quaternion(0, ℝ³(1, 0, 0)))
 
 
-p₁, p₂, p₃, p₄ = typeof(plane), typeof(sprite), size(sprite), size(plane)
-
-
-@test p₁ == Array{Float64,3}
-@test p₂ == Array{ℝ³,1}
-@test p₃ == (6,)
-@test p₄ == (2, 2, 3)
+@test typeof(triad) == Array{ℝ³,1}
+@test size(triad) == (6,)
+@test typeof(torus) == Array{ℝ³,2}
+@test size(torus) == (36, 36)
+@test typeof(sphere) == Array{ℝ³,2}
+@test size(sphere) == (36, 36)
+@test typeof(cylinder) == Array{ℝ³,2}
+@test size(cylinder) == (36, 36)
+@test typeof(box) == Array{ℝ³,2}
+@test size(box) == (4, 4)
+@test typeof(plane) == Array{Float64,3}
+@test size(plane) == (2, 2, 3)
