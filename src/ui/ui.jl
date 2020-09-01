3fbb08417acb5354b42d875ccb5808389d72df71
Mohammad Javad Azadi <mjazadi42@gmail.com>
2020-09-01 17:47:14 +0430

    Added a dispatch for when 2-surface segments differ in different axes, and a dispatch for taking a bitmap image color.

--- src/ui/ui.jl
+++ src/ui/ui.jl
@@ -38,6 +38,29 @@ end
 
 
 """
+    getsurface(observables, segments1, segments2)
+
+Get the array of points of a 2-surface with the given tuple of `observables`, and the number
+of `segments1` and `segments2`.
+"""
+function getsurface(observable::Tuple{Observables.Observable{Array{Float64,2}},
+                                      Observables.Observable{Array{Float64,2}},
+                                      Observables.Observable{Array{Float64,2}}},
+                    segments1::Int,
+                    segments2::Int)
+    y₁, y₂, y₃ = map(x -> Observables.to_value(x), observable)
+    value = Array{ℝ³,2}(undef, segments1, segments2)
+    for i in 1:segments1
+        for j in 1:segments2
+            index = (j - 1) * segments1 + i
+            value[i, j] = ℝ³(y₁[index], y₂[index], y₃[index])
+        end
+    end
+    value
+end
+
+
+"""
     buildsurface(scene, value, color)
 
 Build a surface with the given `scene`, `value` and `color`. An optional argument is
@@ -72,6 +95,25 @@ function buildsurface(scene::AbstractPlotting.Scene,
     AbstractPlotting.surface!(scene, x, y, z, color = color, transparency = transparency)
     x, y, z
 end
+
+
+"""
+    buildsurface(scene, value, color)
+
+Build a surface with the given `scene`, `value` and `color`. An optional argument is
+`transparency`.
+"""
+function buildsurface(scene::AbstractPlotting.Scene,
+                      value::Array{ℝ³,2},
+                      color::Any;
+                      transparency::Bool = false)
+    x = Observables.Observable(map(x -> vec(x)[1] , value))
+    y = Observables.Observable(map(x -> vec(x)[2] , value))
+    z = Observables.Observable(map(x -> vec(x)[3] , value))
+    AbstractPlotting.surface!(scene, x, y, z, color = color, transparency = transparency)
+    x, y, z
+end
+
 
 """
     updatesurface(value, observable)
--- test/ui_tests/ui_tests.jl
+++ test/ui_tests/ui_tests.jl
@@ -1,3 +1,4 @@
+import FileIO
 import Observables
 import AbstractPlotting
 
@@ -21,13 +22,60 @@ value2 = getsurface(observable, segments)
 @test isapprox(value1, value2, atol = TOLERANCE)
 
 
+## getsurface with different segments in the coordinate chart axes
+
+number = rand(5:10)
+points = [Geographic(rand() * 2pi - pi, rand() * pi - pi / 2) for i in 1:number]
+top = U1(rand() * 2pi - pi)
+bottom = U1(rand() * 2pi - pi)
+s3rotation = Quaternion(rand() * 2pi - pi, ℝ³(rand(3)))
+config = Biquaternion(Quaternion(rand() * 2pi - pi, ℝ³(rand(3))), ℝ³(rand(3)))
+segments = rand(5:10)
+value1 = constructwhirl(points,
+                        top = top,
+                        bottom = bottom,
+                        s3rotation = s3rotation,
+                        config = config,
+                        segments = segments)
+
+color = fill(AbstractPlotting.RGBAf0(rand(4)...), segments, number)
+transparency = rand(1:2) == 1 ? true : false
+observable = buildsurface(scene, value1, color, transparency = transparency)
+value2 = getsurface(observable, segments, number)
+
+@test typeof(observable) == Tuple{Observables.Observable{Array{Float64,2}},
+                                  Observables.Observable{Array{Float64,2}},
+                                  Observables.Observable{Array{Float64,2}}}
+@test typeof(value2) == Array{ℝ³,2}
+@test isapprox(value1, value2, atol = TOLERANCE)
+
 ## builsurface with "observable" color array argument
 
 
 observablecolor = Observables.Observable(fill(AbstractPlotting.RGBAf0(rand(4)...),
                                               segments,
                                               segments))
+segments = rand(5:10)
+radius = 1 + rand()
+value1 = constructsphere(q, radius, segments = segments)
 observable = buildsurface(scene, value1, observablecolor, transparency = transparency)
+value2 = getsurface(observable, segments)
+
+@test typeof(observable) == Tuple{Observables.Observable{Array{Float64,2}},
+                                  Observables.Observable{Array{Float64,2}},
+                                  Observables.Observable{Array{Float64,2}}}
+@test typeof(value2) == Array{ℝ³,2}
+@test isapprox(value1, value2, atol = TOLERANCE)
+
+
+## builsurface with image as color
+
+
+color = FileIO.load("data/basemap90grid.png")
+segments = rand(5:10)
+radius = 1 + rand()
+value1 = constructsphere(q, radius, segments = segments)
+observable = buildsurface(scene, value1, color, transparency = transparency)
 value2 = getsurface(observable, segments)
 
 @test typeof(observable) == Tuple{Observables.Observable{Array{Float64,2}},
