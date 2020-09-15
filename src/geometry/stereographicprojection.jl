c03307f3278d2abe33bddd2ce2b9bb360b041c3d
Mohammad Javad Azadi <mjazadi42@gmail.com>
2020-09-15 17:36:12 +0430

    Added a new generic gunction and a new dispatch.

--- src/geometry/stereographicprojection.jl
+++ src/geometry/stereographicprojection.jl
@@ -1,10 +1,15 @@
-export σmap
+export λmap
+export compressedλmap
 
 
 """
-    σmap(s)
+    λmap(s)
 
 Map from S³ into ℝ³ using stereographic projection with the given point `s`.
 """
-σmap(q::Quaternion) = ℝ³(vec(q)[1:3] ./ (1 - vec(q)[4]))
-σmap(s::S³) = σmap(Quaternion(s))
+λmap(q::Quaternion) = ℝ³(vec(q)[1:3] ./ (1 - vec(q)[4]))
+compressedλmap(q::Quaternion) = begin
+    sigmoid(x::ℝ³) = exp.(vec(x)) ./ (1 .+ exp.(vec(x)))
+    ℝ³(2 .* (sigmoid(λmap(q)) .- 0.5))
+end
+λmap(s::S³) = λmap(Quaternion(s))
--- test/geometry_tests/stereographicprojection_tests.jl
+++ test/geometry_tests/stereographicprojection_tests.jl
@@ -1,4 +1,4 @@
 q = Quaternion(1, 0, 0, 0)
-r = σmap(q)
+r = λmap(q)
 
 @test isapprox(r, ℝ³(1, 0, 0))
