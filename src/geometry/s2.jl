f13eda6828f9eed7fdb7e70798447cba6fc62fad
Mohammad Javad Azadi <mjazadi42@gmail.com>
2020-09-15 17:33:10 +0430

    Fixed an inconsistency between different coordinate chart transformations.

--- src/geometry/s2.jl
+++ src/geometry/s2.jl
@@ -27,15 +27,13 @@ end
 fields: x, y and z.
 """
 struct Cartesian <: S²
-    x::Float64
-    y::Float64
-    z::Float64
-    Cartesian(x::Real, y::Real, z::Real) = new(float(x), float(y), float(z))
+    r::ℝ³
+    Cartesian(r::ℝ³) = new(r)
+    Cartesian(x::Real, y::Real, z::Real) = Cartesian(ℝ³(x, y, z))
     Cartesian(a::Array) = begin
         @assert(length(a) == 3, "The length of the input vector must be exactly 3.")
         Cartesian(a...)
     end
-    Cartesian(r::ℝ³) = Cartesian(vec(r))
 end
 
 
@@ -50,7 +48,7 @@ struct Spherical <: S²
     θ::Float64
     Spherical(r::Real, ϕ::Real, θ::Real) = begin
         @assert(r ≥ 0, "r must be non-negative.")
-        @assert(0 ≤ ϕ ≤ 2π, "ϕ must be in [0, 2π].")
+        @assert(-π ≤ ϕ ≤ π, "ϕ must be in [-π, π].")
         @assert(0 ≤ θ ≤ π, "θ must be in [0, π].")
         new(float(r), float(ϕ), float(θ))
     end
@@ -64,15 +62,17 @@ end
 """
     Represents a point in the geographic coordinate system.
 
-fields: ϕ and θ.
+fields: r, ϕ and θ.
 """
 struct Geographic <: S²
+    r::Float64
     ϕ::Float64
     θ::Float64
-    Geographic(ϕ::Real, θ::Real) = begin
-        @assert(-π ≤ ϕ ≤ π, "ϕ must be in [-π, π].")
-        @assert(-π/2 ≤ θ ≤ π/2, "θ must be in [-π/2, π/2].")
-        new(float(ϕ), float(θ))
+    Geographic(r::Real, ϕ::Real, θ::Real) = begin
+        @assert(r ≥ 0, "r must be non-negative: $r")
+        @assert(-π ≤ ϕ ≤ π, "ϕ must be in [-π, π]: $ϕ")
+        @assert(-π/2 ≤ θ ≤ π/2, "θ must be in [-π/2, π/2]: $θ")
+        new(float(r), float(ϕ), float(θ))
     end
     Geographic(a::Array) = begin
         @assert(length(a) == 2, "The length of the input vector must be exactly 2.")
@@ -85,12 +85,12 @@ Base.real(cl::ComplexLine) = real(cl.z)
 Base.imag(cl::ComplexLine) = imag(cl.z)
 Base.abs(cl::ComplexLine) = abs(cl.z)
 Base.angle(cl::ComplexLine) = angle(cl.z)
-Base.conj(cl::ComplexLine) = conj(cl.z)
+Base.conj(cl::ComplexLine) = ComplexLine(conj(cl.z))
 
-Base.vec(cl::ComplexLine) = [cl.z; conj(cl)]
-Base.vec(c::Cartesian) = [c.x; c.y; c.z]
+Base.vec(cl::ComplexLine) = [cl.z; conj(cl).z]
+Base.vec(c::Cartesian) = vec(c.r)
 Base.vec(s::Spherical) = [s.r; s.ϕ; s.θ]
-Base.vec(g::Geographic) = [g.ϕ; g.θ]
+Base.vec(g::Geographic) = [g.r; g.ϕ; g.θ]
 
 Base.isapprox(c1::Cartesian, c2::Cartesian) = isapprox(vec(c1), vec(c2))
 Base.isapprox(s1::S², s2::S²) = isapprox(Cartesian(s1), Cartesian(s2))
@@ -105,15 +105,18 @@ Base.isapprox(a::Array{<:S²,N} where N,
     return true
 end
 
-ℝ³(c::Cartesian) = ℝ³(vec(c))
-ℝ³(s::S²) = ℝ³(Cartesian(s))
+ℝ³(c::Cartesian) = c.r
+ℝ³(s::S²) = Cartesian(s).r
 
-norm(c::Cartesian) = norm(ℝ³(c))
+norm(c::Cartesian) = norm(c.r)
 
-ComplexLine(r3::ℝ³) = ComplexLine(Cartesian(r3))
-ComplexLine(s::Spherical) = ComplexLine(cot(s.θ / 2) * exp(im * s.ϕ))
-ComplexLine(c::Cartesian) = ComplexLine((c.x + im * c.y) / (1 - c.z))
-ComplexLine(g::Geographic) = ComplexLine(Spherical(g))
+ComplexLine(c::Cartesian) = begin
+    x, y, z = vec(c)
+    ComplexLine(x / (1 - z) + im * y / (1 - z))
+end
+ComplexLine(r::ℝ³) = ComplexLine(Cartesian(r))
+ComplexLine(s::Spherical) = ComplexLine(Cartesian(s))
+ComplexLine(g::Geographic) = ComplexLine(Cartesian(g))
 
 Cartesian(cl::ComplexLine) = begin
     x, y = real(cl), imag(cl)
@@ -121,30 +124,35 @@ Cartesian(cl::ComplexLine) = begin
     Cartesian(2x / d, 2y / d, (d - 2) / d)
 end
 Cartesian(s::Spherical) = begin
-    r, ϕ, θ = s.r, s.ϕ, s.θ
-    Cartesian(r * sin(θ) * cos(ϕ), r * sin(θ) * sin(ϕ), r * cos(θ))
+    r, ϕ, θ = vec(s)
+    x = r * sin(θ) * cos(ϕ)
+    y = r * sin(θ) * sin(ϕ)
+    z = r * cos(θ)
+    Cartesian(x, y, z)
 end
-Cartesian(g::Geographic) = Cartesian(Spherical(g))
+Cartesian(g::Geographic) = begin
+    r, ϕ, θ = vec(g)
+    x = r * cos(θ) * cos(ϕ)
+    y = r * cos(θ) * sin(ϕ)
+    z = r * sin(θ)
+    Cartesian(x, y, z)
+end
 
-Spherical(cl::ComplexLine) = Spherical([1; angle(cl); 2acot(abs(cl))])
 Spherical(c::Cartesian) = begin
     x, y, z = vec(c)
-    if x > 0
-        ϕ = atan(y / x)
-    elseif y > 0
-        ϕ = atan(y / x) + pi
-    else
-        ϕ = atan(y / x) - pi
-    end
     r = norm(c)
-    θ = asin(z / r)
-    Spherical(r, ϕ, pi/2 - θ)
+    Spherical(r, atan(y, x), acos(z / r))
 end
-Spherical(g::Geographic) = Spherical(1, g.ϕ + pi, pi/2 - g.θ)
+Spherical(cl::ComplexLine) = Spherical(Cartesian(cl))
+Spherical(g::Geographic) = Spherical(g.r, g.ϕ, pi / 2 - g.θ)
 Spherical(r::ℝ³) = Spherical(Cartesian(r))
 
 Geographic(g::Geographic) = g
-Geographic(s::Spherical) = Geographic(s.ϕ - pi, pi/2 - s.θ)
-Geographic(r::ComplexLine) = Geographic(Spherical(r))
-Geographic(c::Cartesian) = Geographic(ComplexLine(c))
+Geographic(s::Spherical) = Geographic(s.r, s.ϕ, pi / 2 - s.θ)
+Geographic(c::Cartesian) = begin
+    x, y, z = vec(c)
+    r = norm(c)
+    Geographic(r, atan(y, x), asin(z / r))
+end
+Geographic(r::ComplexLine) = Geographic(Cartesian(r))
 Geographic(r::ℝ³) = Geographic(Cartesian(r))
