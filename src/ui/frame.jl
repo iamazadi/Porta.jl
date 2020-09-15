fc3c5b8e700f8ca221d98d116ba5c64fb9a7d8f8
Mohammad Javad Azadi <mjazadi42@gmail.com>
2020-09-15 17:37:54 +0430

    Added a new parameter and changed the interface for clarity.

--- src/ui/frame.jl
+++ src/ui/frame.jl
@@ -9,10 +9,12 @@ export update
 """
     Represents a frame.
 
-fields: circle, color, s3rotation, config, segments and observable.
+fields: circle, s2tos3map, s2tos2map, color, s3rotation, config, segments and observable.
 """
 mutable struct Frame <: Sprite
     circle::S¹
+    s2tos3map::Any
+    s2tos2map::Any
     color::Any
     s3rotation::S³
     config::Biquaternion
@@ -24,24 +26,30 @@ end
 
 
 """
-    Frame(scene, circle, color, [s3rotation, [config, [segments, [transparency]]]])
+    Frame(scene, circle, s2tos3map, s2tos2map, color,
+          [s3rotation, [config, [segments, [transparency]]]])
 
-Construct a Frame with the given `scene`, `circle` in the fiber space, S³ rotation
-`s3rotation`, configuration `config`, the number of `segments`, `color` and `transparency`.
+Construct a Frame with the given `scene`, `circle` in the fiber space, function `f` that
+takes the base space to itself, S³ rotation `s3rotation`, configuration `config`, the number
+of `segments`, `color` and `transparency`.
 """
 function Frame(scene::AbstractPlotting.Scene,
                circle::S¹,
+               s2tos3map,
+               s2tos2map,
                color::Any;
                s3rotation::S³ = Quaternion(1, 0, 0, 0),
                config::Biquaternion = Biquaternion(ℝ³(0, 0, 0)),
                segments::Int = 36,
                transparency::Bool = false)
     frame = constructframe(circle,
+                           s2tos3map,
+                           s2tos2map,
                            s3rotation = s3rotation,
                            config = config,
                            segments = segments)
     observable = buildsurface(scene, frame, color, transparency = transparency)
-    Frame(circle, color, s3rotation, config, segments, observable)
+    Frame(circle, s2tos3map, s2tos2map, color, s3rotation, config, segments, observable)
 end
 
 
@@ -54,6 +62,28 @@ space.
 function update(frame::Frame, circle::S¹)
     frame.circle = circle
     value = constructframe(frame.circle,
+                           frame.s2tos3map,
+                           frame.s2tos2map,
+                           s3rotation = frame.s3rotation,
+                           config = frame.config,
+                           segments = frame.segments)
+    updatesurface(value, frame.observable)
+end
+
+
+"""
+    update(frame, s2tos3map, s2tos2map)
+
+Update a Frame by changing its observable with the given `frame` and map `s2tos3map` from
+the base space into the total space, f: S² → S³, and also the map `s2tos2map` from the base
+space into itself, f: S² → S².
+"""
+function update(frame::Frame, s2tos3map::Any, s2tos2map::Any)
+    frame.s2tos3map = s2tos3map
+    frame.s2tos2map = s2tos2map
+    value = constructframe(frame.circle,
+                           frame.s2tos3map,
+                           frame.s2tos2map,
                            s3rotation = frame.s3rotation,
                            config = frame.config,
                            segments = frame.segments)
@@ -70,6 +100,8 @@ Update a Frame by changing its observable with the given `frame` and S³ rotatio
 function update(frame::Frame, s3rotation::S³)
     frame.s3rotation = s3rotation
     value = constructframe(frame.circle,
+                           frame.s2tos3map,
+                           frame.s2tos2map,
                            s3rotation = frame.s3rotation,
                            config = frame.config,
                            segments = frame.segments)
@@ -85,6 +117,8 @@ Update a Frame by changing its observable with the given `frame` and configurati
 function update(frame::Frame, config::Biquaternion)
     frame.config = config
     value = constructframe(frame.circle,
+                           frame.s2tos3map,
+                           frame.s2tos2map,
                            s3rotation = frame.s3rotation,
                            config = frame.config,
                            segments = frame.segments)
--- test/ui_tests/frame_tests.jl
+++ test/ui_tests/frame_tests.jl
@@ -3,15 +3,26 @@ import AbstractPlotting
 import FileIO
 
 
+# Map from S² into its upper hemisphere
+s2tos2map(b::S²) = begin
+    p = Geographic(b)
+    r = sqrt((1 - sin(p.θ)) / 2)
+    Geographic(p.r, r * cos(p.ϕ), r * sin(p.ϕ))
+end
+
+
 scene = AbstractPlotting.Scene()
 circle = U1(rand() * 2pi - pi)
 s3rotation = Quaternion(rand() * 2pi - pi, ℝ³(rand(3)))
 config = Biquaternion(Quaternion(rand() * 2pi - pi, ℝ³(rand(3))), ℝ³(rand(3)))
 segments = rand(5:10)
-color = FileIO.load("data/basemap90grid.png")
+color = FileIO.load("../data/basemap_inferno.png")
 transparency = rand(1:2) == 1 ? true : false
+s2tos3map = rand() > 0.5 ? σmap : τmap
 frame = Frame(scene,
               circle,
+              s2tos3map,
+              s2tos2map,
               color,
               s3rotation = s3rotation,
               config = config,
@@ -25,6 +36,18 @@ circle2 = U1(rand() * 2pi - pi)
 update(frame, circle2)
 
 @test isapprox(frame.circle, circle2)
+
+
+## update maps f: S² → S³ and g: S² → S²
+
+
+s2tos3map = rand() > 0.5 ? σmap : τmap
+s2tos2map′(x) = Geographic(0.5 * x.r, 0.5 * x.ϕ, 0.5 * x.θ)
+update(frame, s2tos3map, s2tos2map′)
+p = Geographic(rand(), rand() * 2pi - pi, rand() * pi - pi / 2)
+
+@test isapprox(frame.s2tos3map(p), s2tos3map(p))
+@test isapprox(frame.s2tos2map(p), s2tos2map′(p))
 
 
 ## update S³ rotation
