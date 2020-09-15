a0ed871ddba44b5008591f2703375b312d2d10cc
Mohammad Javad Azadi <mjazadi42@gmail.com>
2020-09-15 17:41:06 +0430

    Added a new unit test and also changed the way a Geographic is instantiated.

--- test/geometry_tests/s2_tests.jl
+++ test/geometry_tests/s2_tests.jl
@@ -2,7 +2,7 @@ cl = ComplexLine(100rand() + im * 100rand())
 s = Spherical(cl)
 c = Cartesian(cl)
 g = Geographic(cl)
-r3 = ℝ³(1, 0, 0)
+r3 = normalize(ℝ³(rand(3)))
 
 
 @test isapprox(cl, ComplexLine(s))
@@ -14,13 +14,14 @@ r3 = ℝ³(1, 0, 0)
 @test isapprox(c, Cartesian(g))
 @test isapprox(g, Geographic(s))
 @test isapprox(g, Geographic(c))
+@test isapprox(g, Geographic(Cartesian(ℝ³(Cartesian(g)))))
 
 @test isapprox(ℝ³(cl), ℝ³(c))
 @test isapprox(ComplexLine(r3), ComplexLine(Cartesian(r3)))
 
 number = rand(5:10)
-a = [Geographic(rand() * 2pi - pi, rand() * pi - pi / 2) for i in 1:number]
-b = [Geographic(rand() * 2pi - pi, rand() * pi - pi / 2) for i in 1:number]
+a = [Geographic(rand(), rand() * 2pi - pi, rand() * pi - pi / 2,) for i in 1:number]
+b = [Geographic(rand(), rand() * 2pi - pi, rand() * pi - pi / 2) for i in 1:number]
 
 @test isapprox(a, a, atol = TOLERANCE)
 @test isapprox(a, b, atol = TOLERANCE) == false
