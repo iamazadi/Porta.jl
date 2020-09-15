ed3760827dc11d96b2381b79e0608e4a8643de20
Mohammad Javad Azadi <mjazadi42@gmail.com>
2020-09-15 17:39:03 +0430

    Added a new field and updated the interface.

--- src/ui/whirl.jl
+++ src/ui/whirl.jl
@@ -9,10 +9,13 @@ export update
 """
     Represents a whirl.
 
-fields: points, top, bottom, s3rotation, config, segments, color and observable.
+fields: points, s2tos2map, s2tos3map, top, bottom, s3rotation, config, segments, color and
+observable.
 """
 mutable struct Whirl <: Sprite
     points::Array{<:S²,1}
+    s2tos3map::Any
+    s2tos2map::Any
     top::S¹
     bottom::S¹
     s3rotation::S³
@@ -28,14 +31,18 @@ end
 """
     Whirl(scene,
           points,
+          s2tos3map,
+          s2tos2map,
           [top, [bottom, [s3rotation, [config, [segments, [color, [transparency]]]]]]])
 
-Construct a whirl with the given `scene`, `points` in the base space, `top`, `bottom`,
-S³ rotation `s3rotation`, configuration `config`, the number of `segments`, `color` and
-`transparency`.
+Construct a whirl with the given `scene`, `points` in the base space, `s2tos3map`,
+`s2tos2map`, `top`, `bottom`, S³ rotation `s3rotation`, configuration `config`, the number
+of `segments`, `color` and `transparency`.
 """
 function Whirl(scene::AbstractPlotting.Scene,
-               points::Array{<:S²,1};
+               points::Array{<:S²,1},
+               s2tos3map,
+               s2tos2map;
                top::S¹ = U1(-pi),
                bottom::S¹ = U1(pi),
                s3rotation::S³ = Quaternion(1, 0, 0, 0),
@@ -47,6 +54,8 @@ function Whirl(scene::AbstractPlotting.Scene,
                                                                         0.5),
                transparency::Bool = false)
     whirl = constructwhirl(points,
+                           s2tos3map,
+                           s2tos2map,
                            top = top,
                            bottom = bottom,
                            s3rotation = s3rotation,
@@ -54,7 +63,8 @@ function Whirl(scene::AbstractPlotting.Scene,
                            segments = segments)
     colorarray = fill(color, segments, segments)
     observable = buildsurface(scene, whirl, colorarray, transparency = transparency)
-    Whirl(points, top, bottom, s3rotation, config, segments, color, observable)
+    Whirl(points, s2tos3map, s2tos2map, top, bottom, s3rotation, config, segments, color,
+          observable)
 end
 
 
@@ -69,6 +79,30 @@ function update(whirl::Whirl, points::Array{<:S²,1})
             "The number of the given points must be equal to what it was before.")
     whirl.points = points
     value = constructwhirl(whirl.points,
+                           whirl.s2tos3map,
+                           whirl.s2tos2map,
+                           top = whirl.top,
+                           bottom = whirl.bottom,
+                           s3rotation = whirl.s3rotation,
+                           config = whirl.config,
+                           segments = whirl.segments)
+    updatesurface(value, whirl.observable)
+end
+
+
+"""
+    update(whirl, s2tos3map, s2tos2map)
+
+Update a Whirl by changing its observable with the given `whirl` and map `s2tos3map` from
+the base space into the total space, f: S² → S³, and also the map `s2tos2map` from the base
+space into itself, f: S² → S².
+"""
+function update(whirl::Whirl, s2tos3map::Any, s2tos2map::Any)
+    whirl.s2tos3map = s2tos3map
+    whirl.s2tos2map = s2tos2map
+    value = constructwhirl(whirl.points,
+                           whirl.s2tos3map,
+                           whirl.s2tos2map,
                            top = whirl.top,
                            bottom = whirl.bottom,
                            s3rotation = whirl.s3rotation,
@@ -87,6 +121,8 @@ function update(whirl::Whirl, top::S¹, bottom::S¹)
     whirl.top = top
     whirl.bottom = bottom
     value = constructwhirl(whirl.points,
+                           whirl.s2tos3map,
+                           whirl.s2tos2map,
                            top = whirl.top,
                            bottom = whirl.bottom,
                            s3rotation = whirl.s3rotation,
@@ -105,6 +141,8 @@ Update a Whirl by changing its observable with the given `whirl` and S³ rotatio
 function update(whirl::Whirl, s3rotation::S³)
     whirl.s3rotation = s3rotation
     value = constructwhirl(whirl.points,
+                           whirl.s2tos3map,
+                           whirl.s2tos2map,
                            top = whirl.top,
                            bottom = whirl.bottom,
                            s3rotation = whirl.s3rotation,
@@ -122,6 +160,8 @@ Update a Whirl by changing its observable with the given `whirl` and configurati
 function update(whirl::Whirl, config::Biquaternion)
     whirl.config = config
     value = constructwhirl(whirl.points,
+                           whirl.s2tos3map,
+                           whirl.s2tos2map,
                            top = whirl.top,
                            bottom = whirl.bottom,
                            s3rotation = whirl.s3rotation,
--- test/ui_tests/whirl_tests.jl
+++ test/ui_tests/whirl_tests.jl
@@ -2,9 +2,17 @@ import Observables
 import AbstractPlotting
 
 
+# Map from S² into its upper hemisphere
+s2tos2map(b::S²) = begin
+    p = Geographic(b)
+    r = sqrt((1 - sin(p.θ)) / 2)
+    Geographic(p.r, r * cos(p.ϕ), r * sin(p.ϕ))
+end
+
+
 scene = AbstractPlotting.Scene()
 number = rand(5:10)
-points = [Geographic(rand() * 2pi - pi, rand() * pi - pi / 2) for i in 1:number]
+points = [Geographic(rand(), rand() * 2pi - pi, rand() * pi - pi / 2) for i in 1:number]
 top = U1(rand() * 2pi - pi)
 bottom = U1(rand() * 2pi - pi)
 s3rotation = Quaternion(rand() * 2pi - pi, ℝ³(rand(3)))
@@ -12,8 +20,11 @@ config = Biquaternion(Quaternion(rand() * 2pi - pi, ℝ³(rand(3))), ℝ³(rand(
 segments = rand(5:10)
 color = AbstractPlotting.RGBAf0(rand(4)...)
 transparency = rand(1:2) == 1 ? true : false
+s2tos3map = rand() > 0.5 ? σmap : τmap
 whirl = Whirl(scene,
               points,
+              s2tos3map,
+              s2tos2map,
               top = top,
               bottom = bottom,
               s3rotation = s3rotation,
@@ -25,10 +36,23 @@ whirl = Whirl(scene,
 
 ## update points
 
-points2 = [Geographic(rand() * 2pi - pi, rand() * pi - pi / 2) for i in 1:number]
+points2 = [Geographic(rand(), rand() * 2pi - pi, rand() * pi - pi / 2) for i in 1:number]
 update(whirl, points2)
 
 @test isapprox(whirl.points, points2)
+
+
+# update maps f: S² → S³ and g: S² → S²
+
+
+s2tos3map = rand() > 0.5 ? σmap : τmap
+s2tos2map′(x) = Geographic(0.5 * x.r, 0.5 * x.ϕ, 0.5 * x.θ)
+update(whirl, s2tos3map, s2tos2map′)
+p = Geographic(rand(), rand() * 2pi - pi, rand() * pi - pi / 2)
+
+@test isapprox(whirl.s2tos3map(p), s2tos3map(p))
+@test isapprox(whirl.s2tos2map(p), s2tos2map′(p))
+
 
 ## update top and bottom
 
