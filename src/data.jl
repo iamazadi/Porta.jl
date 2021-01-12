6c0b4bff8acdbe89f4e91c13c91bd622e5f3dce2
MohammadJavad Azadi <mjazadi42@gmail.com>
2021-01-12 19:53:16 +0330

    Removed old redundant files.

--- src/data.jl
+++
@@ -1,9 +0,0 @@
-export loadordownload
-
-import FileIO
-
-
-function loadordownload(localfname, url)
-    path = "data/" * localfname
-    FileIO.isfile(path) ? FileIO.load(path) : FileIO.load(FileIO.download(url, localfname))
-end
