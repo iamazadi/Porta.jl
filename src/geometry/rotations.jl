30afc3b114c593ed180443f0d4052aaaf89e3d7c
Mohammad Javad Azadi <mjazadi42@gmail.com>
2022-03-18 09:37:03 +0100

    Added a function for applying Lie algebra elements to Lie group elements.

--- src/geometry/rotations.jl
+++ src/geometry/rotations.jl
@@ -4,6 +4,7 @@ export applyconfig
 
 
 rotate(g::Quaternion, q::Quaternion) = g * q
+rotate(M::Array{<:Complex,2}, q::ComplexPlane) = ComplexPlane(M * vec(q))
 rotate(p::ℝ³, q::Quaternion) = ℝ³(vec(q * Quaternion([0; vec(p)]) * conj(q))[2:4])
 rotate(p::Array{ℝ³}, q::Quaternion) = map(x -> rotate(x, q), p)
 getrotation(i::ℝ³, n::ℝ³) = begin
