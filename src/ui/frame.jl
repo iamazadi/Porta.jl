78d0074d2cb61e6b479f8cad63c867fd74c133af
Mohammad Javad Azadi <mjazadi42@gmail.com>
2022-03-28 15:07:33 +0200

    Improved the conceptual model of Frame by constructing it from a section.

--- src/ui/frame.jl
+++ src/ui/frame.jl
@@ -9,15 +9,12 @@ export update
 """
     Represents a frame.
 
-fields: circle, s2tos3map, s2tos2map, color, s3rotation, config, segments and observable.
+fields: section, color, configuration, segments and observable.
 """
 mutable struct Frame <: Sprite
-    circle::S¹
-    s2tos3map::Any
-    s2tos2map::Any
+    section::Any
     color::Any
-    s3rotation::S³
-    config::Biquaternion
+    configuration::Biquaternion
     segments::Int
     observable::Tuple{Observables.Observable{Array{Float64,2}},
                       Observables.Observable{Array{Float64,2}},
@@ -26,101 +23,41 @@ end
 
 
 """
-    Frame(scene, circle, s2tos3map, s2tos2map, color,
-          [s3rotation, [config, [segments, [transparency]]]])
+    Frame(scene, gauge, color, [configuration, [segments, [transparency]]])
 
-Construct a Frame with the given `scene`, `circle` in the fiber space, function `f` that
-takes the base space to itself, S³ rotation `s3rotation`, configuration `config`, the number
-of `segments`, `color` and `transparency`.
+Construct a Frame with the given `scene`, `section`, `configuration`, `segments`, `color` and `transparency`.
 """
 function Frame(scene::Makie.Scene,
-               circle::S¹,
-               s2tos3map,
-               s2tos2map,
+               section::Any,
                color::Any;
-               s3rotation::S³ = Quaternion(1, 0, 0, 0),
-               config::Biquaternion = Biquaternion(ℝ³(0, 0, 0)),
+               configuration::Biquaternion = Biquaternion(ℝ³(0, 0, 0)),
                segments::Int = 36,
                transparency::Bool = false)
-    frame = constructframe(circle,
-                           s2tos3map,
-                           s2tos2map,
-                           s3rotation = s3rotation,
-                           config = config,
-                           segments = segments)
+    frame = constructframe(section, configuration, segments)
     observable = buildsurface(scene, frame, color, transparency = transparency)
-    Frame(circle, s2tos3map, s2tos2map, color, s3rotation, config, segments, observable)
+    Frame(section, color, configuration, segments, observable)
 end
 
 
 """
-    update(frame, circle)
+    update(frame, section)
 
-Update a Frame by changing its observable with the given `frame` and `circle` in the fiber
-space.
+Update a Frame by changing its observable with the given `frame` and `section`.
 """
-function update(frame::Frame, circle::S¹)
-    frame.circle = circle
-    value = constructframe(frame.circle,
-                           frame.s2tos3map,
-                           frame.s2tos2map,
-                           s3rotation = frame.s3rotation,
-                           config = frame.config,
-                           segments = frame.segments)
+function update(frame::Frame, section::Any)
+    frame.section = section
+    value = constructframe(frame.section, frame.configuration, frame.segments)
     updatesurface(value, frame.observable)
 end
 
 
 """
-    update(frame, s2tos3map, s2tos2map)
+    update(frame, configuration)
 
-Update a Frame by changing its observable with the given `frame` and map `s2tos3map` from
-the base space into the total space, f: S² → S³, and also the map `s2tos2map` from the base
-space into itself, f: S² → S².
+Update a Frame by changing its observable with the given `frame` and 1configuration`.
 """
-function update(frame::Frame, s2tos3map::Any, s2tos2map::Any)
-    frame.s2tos3map = s2tos3map
-    frame.s2tos2map = s2tos2map
-    value = constructframe(frame.circle,
-                           frame.s2tos3map,
-                           frame.s2tos2map,
-                           s3rotation = frame.s3rotation,
-                           config = frame.config,
-                           segments = frame.segments)
-    updatesurface(value, frame.observable)
-end
-
-
-"""
-    update(frame, s3rotation)
-
-Update a Frame by changing its observable with the given `frame` and S³ rotation
-`s3rotation`.
-"""
-function update(frame::Frame, s3rotation::S³)
-    frame.s3rotation = s3rotation
-    value = constructframe(frame.circle,
-                           frame.s2tos3map,
-                           frame.s2tos2map,
-                           s3rotation = frame.s3rotation,
-                           config = frame.config,
-                           segments = frame.segments)
-    updatesurface(value, frame.observable)
-end
-
-
-"""
-    update(frame, config)
-
-Update a Frame by changing its observable with the given `frame` and configuration `config`.
-"""
-function update(frame::Frame, config::Biquaternion)
-    frame.config = config
-    value = constructframe(frame.circle,
-                           frame.s2tos3map,
-                           frame.s2tos2map,
-                           s3rotation = frame.s3rotation,
-                           config = frame.config,
-                           segments = frame.segments)
+function update(frame::Frame, configuration::Biquaternion)
+    frame.configuration = configuration
+    value = constructframe(frame.section, frame.configuration, frame.segments)
     updatesurface(value, frame.observable)
 end
