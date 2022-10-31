30fa6f0ad6b71215496976d907b3002ffa6edf2d
Mohammad Javad Azadi <mjazadi42@gmail.com>
2022-10-31 05:31:26 +0100

    Fixed a condition where the initial and final axes are linearly dependent.

--- src/geometry/rotations.jl
+++ src/geometry/rotations.jl
@@ -8,7 +8,7 @@ rotate(M::Array{<:Complex,2}, q::ComplexPlane) = ComplexPlane(M * vec(q))
 rotate(p::ℝ³, q::Quaternion) = ℝ³(vec(q * Quaternion([0; vec(p)]) * conj(q))[2:4])
 rotate(p::Array{ℝ³}, q::Quaternion) = map(x -> rotate(x, q), p)
 getrotation(i::ℝ³, n::ℝ³) = begin
-    if isapprox(i, n)
+    if isapprox(normalize(i), normalize(n))
         return Quaternion(0, normalize(i))
     end
     u = normalize(cross(i, n))
