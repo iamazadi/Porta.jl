acf571c90250718cfc04b03a52401f0606c0bf8f
Mohammad Javad Azadi <mjazadi42@gmail.com>
2022-11-11 07:23:03 +0100

    Added scale for resizing, and added a dispatch for updating configuration along with other parameters since all call for a construction.

--- src/computergraphics/whirl.jl
+++ src/computergraphics/whirl.jl
@@ -1,3 +1,5 @@
+import FixedPointNumbers
+import ColorTypes
 import Observables
 import Makie
 
@@ -9,7 +11,7 @@ export update
 """
     Represents a whirl.
 
-fields: points, gauge1, gauge2, configuration, segments, color and observable.
+fields: points, gauge1, gauge2, configuration, segments, color, observable and scale.
 """
 mutable struct Whirl <: Sprite
     points::Array{ComplexPlane,1}
@@ -17,33 +19,35 @@ mutable struct Whirl <: Sprite
     gauge2::Array{U1,1}
     configuration::Biquaternion
     segments::Int
-    color::Observables.Observable{Array{Makie.ColorTypes.RGBA{Float32},2}}
+    color::Observables.Observable{Array{Makie.ColorTypes.RGBA{FixedPointNumbers.Normed{UInt8, 8}},2}}
     observable::Tuple{Observables.Observable{Array{Float64,2}},
                       Observables.Observable{Array{Float64,2}},
                       Observables.Observable{Array{Float64,2}}}
+    scale::Float64
 end
 
 
 """
     Whirl(scene,
           points,
-          [gauge1, [gauge2, [configuration, [segments, [color, [transparency]]]]]])
+          [gauge1, [gauge2, [configuration, [segments, [color, [transparency, [scale]]]]]]])
 
-Construct a whirl with the given `scene`, `points`, `section`, `gauge1`, `gauge2`,
-`configuration`, `segments`, `color` and `transparency`.
+Construct a whirl with the given `scene`, `points`, `gauge1`, `gauge2`,
+`configuration`, `segments`, `color`, `transparency` and `scale`.
 """
-function Whirl(scene::Makie.Scene,
+function Whirl(scene::Makie.LScene,
                points::Array{ComplexPlane,1},
                gauge1::Array{U1,1},
                gauge2::Array{U1,1};
                configuration::Biquaternion = Biquaternion(ℝ³(0, 0, 0)),
                segments::Int = 36,
-               color::Makie.RGBAf = Makie.RGBAf(1.0, 0.2705, 0.0, 0.5),
-               transparency::Bool = false)
-    whirl = constructwhirl(points, gauge1, gauge2, configuration, segments)
+               color::Makie.RGBA{FixedPointNumbers.Normed{UInt8, 8}} = Makie.RGBA{FixedPointNumbers.Normed{UInt8, 8}}(1.0, 0.2705, 0.0, 0.5),
+               transparency::Bool = false,
+               scale::Float64 = 1.0)
+    whirl = constructwhirl(points, gauge1, gauge2, configuration, segments, scale)
     colorarray = Observables.Observable(fill(color, size(whirl)...))
     observable = buildsurface(scene, whirl, colorarray, transparency = transparency)
-    Whirl(points, gauge1, gauge2, configuration, segments, colorarray, observable)
+    Whirl(points, gauge1, gauge2, configuration, segments, colorarray, observable, scale)
 end
 
 
@@ -58,7 +62,8 @@ function update(whirl::Whirl, points::Array{ComplexPlane,1})
                            whirl.gauge1,
                            whirl.gauge2,
                            whirl.configuration,
-                           whirl.segments)
+                           whirl.segments,
+                           whild.scale)
     updatesurface(value, whirl.observable)
 end
 
@@ -75,7 +80,8 @@ function update(whirl::Whirl, gauge1::Array{U1,1}, gauge2::Array{U1,1})
                            whirl.gauge1,
                            whirl.gauge2,
                            whirl.configuration,
-                           whirl.segments)
+                           whirl.segments,
+                           whirl.scale)
     updatesurface(value, whirl.observable)
 end
 
@@ -93,7 +99,28 @@ function update(whirl::Whirl, points::Array{ComplexPlane,1}, gauge1::Array{U1,1}
                            whirl.gauge1,
                            whirl.gauge2,
                            whirl.configuration,
-                           whirl.segments)
+                           whirl.segments,
+                           whirl.scale)
+    updatesurface(value, whirl.observable)
+end
+
+
+"""
+    update(whirl, points, gauge1, gauge2, configuration)
+
+Update a Whirl by changing its observable with the given `whirl`, `points`, `gauge1`, `gauge2` and `configuration`.
+"""
+function update(whirl::Whirl, points::Array{ComplexPlane,1}, gauge1::Array{U1,1}, gauge2::Array{U1,1}, configuration::Biquaternion)
+    whirl.points = points
+    whirl.gauge1 = gauge1
+    whirl.gauge2 = gauge2
+    whirl.configuration = configuration
+    value = constructwhirl(whirl.points,
+                           whirl.gauge1,
+                           whirl.gauge2,
+                           whirl.configuration,
+                           whirl.segments,
+                           whirl.scale)
     updatesurface(value, whirl.observable)
 end
 
@@ -109,7 +136,8 @@ function update(whirl::Whirl, configuration::Biquaternion)
                            whirl.gauge1,
                            whirl.gauge2,
                            whirl.configuration,
-                           whirl.segments)
+                           whirl.segments,
+                           whirl.scale)
     updatesurface(value, whirl.observable)
 end
 
@@ -119,6 +147,6 @@ end
 
 Update a Whirl by changing its observable with the given `whirl` and `color`.
 """
-function update(whirl::Whirl, color::Makie.RGBAf)
-    whirl.color[] = fill(color, whirl.segments, length(whirl.points))
+function update(whirl::Whirl, color::Makie.RGBA{FixedPointNumbers.Normed{UInt8, 8}})
+    whirl.color[] = fill(color, length(whirl.points), whirl.segments)
 end
