deabfb9a4a93fc7c6418cd354beed3c6632f46d8
Mohammad Javad Azadi <mjazadi42@gmail.com>
2022-11-11 07:21:24 +0100

    Added scale for resizing, and a dispatch for updating section and configuration since both call for a construction.

--- src/computergraphics/frame.jl
+++ src/computergraphics/frame.jl
@@ -9,7 +9,7 @@ export update
 """
     Represents a frame.
 
-fields: section, color, configuration, segments and observable.
+fields: section, color, configuration, segments, observable and scale.
 """
 mutable struct Frame <: Sprite
     section::Any
@@ -19,23 +19,25 @@ mutable struct Frame <: Sprite
     observable::Tuple{Observables.Observable{Array{Float64,2}},
                       Observables.Observable{Array{Float64,2}},
                       Observables.Observable{Array{Float64,2}}}
+    scale::Float64
 end
 
 
 """
-    Frame(scene, gauge, color, [configuration, [segments, [transparency]]])
+    Frame(scene, gauge, color, [configuration, [segments, [transparency, [scale]]]])
 
-Construct a Frame with the given `scene`, `section`, `configuration`, `segments`, `color` and `transparency`.
+Construct a Frame with the given `scene`, `section`, `configuration`, `segments`, `color`, `transparency` and `scale`.
 """
-function Frame(scene::Makie.Scene,
+function Frame(scene::Makie.LScene,
                section::Any,
                color::Any;
                configuration::Biquaternion = Biquaternion(ℝ³(0, 0, 0)),
                segments::Int = 36,
-               transparency::Bool = false)
-    frame = constructframe(section, configuration, segments)
+               transparency::Bool = false,
+               scale::Float64 = 1.0)
+    frame = constructframe(section, configuration, segments, scale)
     observable = buildsurface(scene, frame, color, transparency = transparency)
-    Frame(section, color, configuration, segments, observable)
+    Frame(section, color, configuration, segments, observable, scale)
 end
 
 
@@ -46,7 +48,7 @@ Update a Frame by changing its observable with the given `frame` and `section`.
 """
 function update(frame::Frame, section::Any)
     frame.section = section
-    value = constructframe(frame.section, frame.configuration, frame.segments)
+    value = constructframe(frame.section, frame.configuration, frame.segments, frame.scale)
     updatesurface(value, frame.observable)
 end
 
@@ -58,6 +60,19 @@ Update a Frame by changing its observable with the given `frame` and 1configurat
 """
 function update(frame::Frame, configuration::Biquaternion)
     frame.configuration = configuration
-    value = constructframe(frame.section, frame.configuration, frame.segments)
+    value = constructframe(frame.section, frame.configuration, frame.segments, frame.scale)
+    updatesurface(value, frame.observable)
+end
+
+
+"""
+    update(frame, section, configuration)
+
+Update a Frame by changing its observable with the given `frame`, `section` and `configuration`.
+"""
+function update(frame::Frame, section::Any, configuration::Biquaternion)
+    frame.section = section
+    frame.configuration = configuration
+    value = constructframe(frame.section, frame.configuration, frame.segments, frame.scale)
     updatesurface(value, frame.observable)
 end
