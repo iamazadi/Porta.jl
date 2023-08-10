f3600a4997a5dbc3e2f410e7cfb7f6212f421171
Mohammad Javad Azadi <mjazadi42@gmail.com>
2023-08-10 08:36:58 +0200

    Refactored the code for unit testing the quaternions algebra and the new UI.

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,62 +2,24 @@ using Test
 using Porta
 
 
-const TOLERANCE = 1e-7
-
-
-logicdir = "logic_tests"
-linearalgebradir = "topology_tests/linearalgebra"
-geometrydir = "geometry_tests"
-bundlesdir = "bundles_tests"
-computergraphicsdir = "computergraphics_tests"
-datadir = "data_tests"
-
+const TOLERANCE = 1e-8
 
 start = time()
 
-
-@time @testset "The Logic Tests" begin
-    include(joinpath(logicdir, "propositionallogic_tests.jl"))
-    include(joinpath(logicdir, "predicatelogic_tests.jl"))
-end
-
-@time @testset "The Linear Algebra Tests" begin
-    include(joinpath(linearalgebradir, "determinant_tests.jl"))
-    include(joinpath(linearalgebradir, "real3_tests.jl"))
-    include(joinpath(linearalgebradir, "real4_tests.jl"))
-end
-
-@time @testset "The Geometry Tests" begin
-    include(joinpath(geometrydir, "s1_tests.jl"))
-    include(joinpath(geometrydir, "s2_tests.jl"))
-    include(joinpath(geometrydir, "s3_tests.jl"))
-    include(joinpath(geometrydir, "biquaternions_tests.jl"))
-    include(joinpath(geometrydir, "stereographicprojection_tests.jl"))
-    include(joinpath(geometrydir, "rotations_tests.jl"))
+@time @testset "The Numbers Tests" begin
+    include("quaternions_tests.jl")
+    include("dualquaternions_tests.jl")
 end
 
 @time @testset "The Bundles Tests" begin
-    include(joinpath(bundlesdir, "clifford_tests.jl"))
+    include("hopfbundle_tests.jl")
 end
 
-
-@time @testset "The UI Tests" begin
-    include(joinpath(computergraphicsdir, "body_tests.jl"))
-    include(joinpath(computergraphicsdir, "ui_tests.jl"))
-    include(joinpath(computergraphicsdir, "arrow_tests.jl"))
-    include(joinpath(computergraphicsdir, "cylinder_tests.jl"))
-    include(joinpath(computergraphicsdir, "sphere_tests.jl"))
-    include(joinpath(computergraphicsdir, "hemisphere_tests.jl"))
-    include(joinpath(computergraphicsdir, "torus_tests.jl"))
-    include(joinpath(computergraphicsdir, "triad_tests.jl"))
-    include(joinpath(computergraphicsdir, "whirl_tests.jl"))
-    include(joinpath(computergraphicsdir, "frame_tests.jl"))
-    include(joinpath(computergraphicsdir, "coloring_tests.jl"))
-end
-
-
-@time @testset "The Data Tests" begin
-    include(joinpath(datadir, "signal_tests.jl"))
+@time @testset "The Graphics Tests" begin
+    include("surface_tests.jl")
+    include("whirl_tests.jl")
+    include("frame_tests.jl")
+    include("earth_tests.jl")
 end
 
 elapsed = time() - start
