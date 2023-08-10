3956c000e8033d68168341f6b3a4328f8611d27f
Mohammad Javad Azadi <mjazadi42@gmail.com>
2023-08-10 08:49:42 +0200

    Added an interface for building and efficiently updating Makie surfaces.

---
+++ src/surface.jl
@@ -0,0 +1,61 @@
+import GLMakie
+
+
+export getsurface
+export buildsurface
+export updatesurface!
+
+
+"""
+    getsurface(observables, segments1, segments2)
+
+Get the matrix of points of a 2-surface with the given tuple of `observables`, and the number of `segments1` and `segments2`.
+"""
+function getsurface(observable::Tuple{GLMakie.Observable{Matrix{Float64}},
+                                      GLMakie.Observable{Matrix{Float64}},
+                                      GLMakie.Observable{Matrix{Float64}}},
+                    segments1::Int,
+                    segments2::Int)
+    y₁, y₂, y₃ = map(x -> GLMakie.to_value(x), observable)
+    value = Matrix{Vector{<:Real}}(undef, segments1, segments2)
+    for i in 1:segments1
+        for j in 1:segments2
+            index = (j - 1) * segments1 + i
+            value[i, j] = [y₁[index]; y₂[index]; y₃[index]]
+        end
+    end
+    value
+end
+
+
+"""
+    buildsurface(scene, value, color)
+
+Build a surface with the given `scene`, `value` and `color`.
+"""
+function buildsurface(scene::GLMakie.LScene,
+                      value::Matrix{<:Vector{<:Real}},
+                      color::Any;
+                      transparency::Bool = false)
+    x = GLMakie.Observable(map(x -> x[1] , value))
+    y = GLMakie.Observable(map(x -> x[2] , value))
+    z = GLMakie.Observable(map(x -> x[3] , value))
+    GLMakie.surface!(scene, x, y, z, color = color, transparency = transparency)
+    x, y, z
+end
+
+
+"""
+    updatesurface!(value, observable)
+
+Update a surface with the given `value` and `observable`.
+"""
+function updatesurface!(value::Matrix{<:Vector{<:Real}},
+                        observable::Tuple{GLMakie.Observable{Matrix{Float64}},
+                                          GLMakie.Observable{Matrix{Float64}},
+                                          GLMakie.Observable{Matrix{Float64}}})
+    x, y, z = observable
+    x[] = map(x -> x[1], value)
+    y[] = map(x -> x[2], value)
+    z[] = map(x -> x[3], value)
+end
---
+++ test/surface_tests.jl
@@ -0,0 +1,50 @@
+import FileIO
+import GLMakie
+
+
+## getsurface and buildsurface
+
+fig = GLMakie.Figure()
+lscene = GLMakie.LScene(fig[1, 1])
+
+segments = rand(5:10)
+
+matrix = Matrix{Vector{Float64}}(undef, segments, segments)
+for i in 1:segments
+    for j in 1:segments
+        matrix[j, i] = rand(3)
+    end
+end
+
+color = fill(GLMakie.RGBAf(rand(4)...), segments, segments)
+observable = buildsurface(lscene, matrix, color, transparency = false)
+_matrix = getsurface(observable, segments, segments)
+
+@test all([isapprox(matrix[i, j], _matrix[i, j]) for i in 1:segments for j in 1:segments])
+
+
+## builsurface with "observable" color array argument
+
+observablecolor = GLMakie.Observable(color)
+observable = buildsurface(lscene, matrix, observablecolor, transparency = true)
+_matrix = getsurface(observable, segments, segments)
+
+@test all([isapprox(matrix[i, j], _matrix[i, j]) for i in 1:segments for j in 1:segments])
+
+
+## builsurface with image as color
+
+color = FileIO.load("../data/basemap_color.png")
+observable = buildsurface(lscene, matrix, color)
+_matrix = getsurface(observable, segments, segments)
+
+@test all([isapprox(matrix[i, j], _matrix[i, j]) for i in 1:segments for j in 1:segments])
+
+
+## updatesurfce
+
+matrix = map(x -> 2 .* x, matrix)
+updatesurface!(matrix, observable)
+_matrix = getsurface(observable, segments, segments)
+
+@test all([isapprox(matrix[i, j], _matrix[i, j]) for i in 1:segments for j in 1:segments])
