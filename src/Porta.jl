10c203d2f761ad5fc81e34f96e7789bea4b9351a
Mohammad Javad Azadi <mjazadi42@gmail.com>
2024-03-12 02:29:15 +0330

    Implemented world and spin vector geometry.

--- src/Porta.jl
+++ src/Porta.jl
@@ -6,13 +6,23 @@ module Porta
 # The default tolerance for comparing the equality of two values.
 const TOLERANCE = 1e-7
 
+directory = "spacetime"
+include(joinpath(directory, "abstractvectorspace.jl"))
+include(joinpath(directory, "real2.jl"))
+include(joinpath(directory, "real3.jl"))
+include(joinpath(directory, "real4.jl"))
+include(joinpath(directory, "determinant.jl"))
+include(joinpath(directory, "tetrad.jl"))
+include(joinpath(directory, "minkowskivectorspace.jl"))
+include(joinpath(directory, "minkowskispacetime.jl"))
+include(joinpath(directory, "spinvector.jl"))
 include("quaternions.jl")
 include("dualquaternions.jl")
 include("hopfbundle.jl")
+include("earth.jl")
+include("rotation.jl")
 include("surface.jl")
 include("whirl.jl")
 include("basemap.jl")
-include("earth.jl")
-include("rotation.jl")
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -6,6 +6,18 @@ const TOLERANCE = 1e-8
 
 start = time()
 
+@time @testset "The Linear Algebra Tests" begin
+    directory = "spacetime_tests"
+    include(joinpath(directory, "real2_tests.jl"))
+    include(joinpath(directory, "real3_tests.jl"))
+    include(joinpath(directory, "real4_tests.jl"))
+    include(joinpath(directory, "determinant_tests.jl"))
+    include(joinpath(directory, "tetrad_tests.jl"))
+    include(joinpath(directory, "minkowskivectorspace_tests.jl"))
+    include(joinpath(directory, "minkowskispacetime_tests.jl"))
+    include(joinpath(directory, "spinvector_tests.jl"))
+end
+
 @time @testset "The Numbers Tests" begin
     include("quaternions_tests.jl")
     include("dualquaternions_tests.jl")
@@ -16,11 +28,11 @@ end
 end
 
 @time @testset "The Graphics Tests" begin
+    include("earth_tests.jl")
+    include("rotation_tests.jl")
     include("surface_tests.jl")
     include("whirl_tests.jl")
     include("basemap_tests.jl")
-    include("earth_tests.jl")
-    include("rotation_tests.jl")
 end
 
 elapsed = time() - start
