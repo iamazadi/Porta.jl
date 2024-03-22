c53ac1ce99a9dc6f49cf9e1293c40344e318e0a5
Mohammad Javad Azadi <mjazadi42@gmail.com>
2024-03-23 02:09:13 +0330

    Defined complex matrix elementary quaternions for relating to spin-matrices.

--- src/quaternions.jl
+++ src/quaternions.jl
@@ -9,26 +9,173 @@ import Base.:-
 import Base.:*
 
 export Quaternion
+export elI
+export eli
+export elj
+export elk
 export mat
+export det
 export K
 export J
 export R
 
 
+# The global constants defining elementary Quaternions
+const elI = Complex.([1 0; 0 1])
+const eli = Complex.([0 im; im 0])
+const elj = Complex.([0 -1; 1 0])
+const elk = Complex.([im 0; 0 -im])
+
+
 """
     Represents a quaternion number.
 
-field: a.
+fields: a, b, c and d.
 """
 struct Quaternion
-    a::ℝ⁴
-    Quaternion(a::ℝ⁴) = new(a)
-    Quaternion(a::Float64, b::Float64, c::Float64, d::Float64) = new(ℝ⁴(a, b, c, d))
-    Quaternion(v::Vector{Float64}) = new(ℝ⁴(v))
-    Quaternion(z::Vector{<:Complex}) = new(ℝ⁴(real(z[1]), real(z[2]), imag(z[1]), imag(z[2])))
-    Quaternion(m::Matrix{Float64}) = Quaternion(ℝ⁴(m[1,1], m[1,2], m[1,3], m[1,4]))
-    Quaternion(θ::Float64, u::ℝ³) = Quaternion(ℝ⁴(cos(θ), vec(sin(θ) * u)...))
+    a::Float64
+    b::Float64
+    c::Float64
+    d::Float64
+    Quaternion(a::Float64, b::Float64, c::Float64, d::Float64) = new(a, b, c, d)
+    Quaternion(a::ℝ⁴) = Quaternion(vec(a)...)
+    Quaternion(v::Vector{Float64}) = Quaternion(v...)
+    Quaternion(z::Vector{<:Complex}) = Quaternion(real(z[1]), real(z[2]), imag(z[1]), imag(z[2]))
+    Quaternion(m::Matrix{Float64}) = Quaternion(m[1,1], m[1,2], m[1,3], m[1,4])
+    Quaternion(M::Matrix{<:Complex}) = begin
+        if isapprox(M, elI)
+            return Quaternion(1.0, 0.0, 0.0, 0.0)
+        elseif isapprox(M, eli)
+            return Quaternion(0.0, 1.0, 0.0, 0.0)
+        elseif isapprox(M, elj)
+            return Quaternion(0.0, 0.0, 1.0, 0.0)
+        elseif isapprox(M, elk)
+            return Quaternion(0.0, 0.0, 0.0, 1.0)
+        else
+            @assert(false, "The direct construction for the given matrix has not been implemented, try other constructors instead.")
+        end
+    end
+    Quaternion(ψ::Float64, u::ℝ³) = begin
+        @assert(isapprox(norm(u), 1.0), "The input vector must have unit norm, but the norm is $(norm(u)).")
+        Quaternion(ℝ⁴(cos(ψ / 2), vec(sin(ψ / 2) * u)...))
+    end
 end
+
+
+"""
+    vec(q)
+ 
+ Reshape the number `q` as a column 4-vector.
+ """
+Base.vec(q::Quaternion) = [q.a; q.b; q.c; q.d]
+
+
+"""
+    show(q)
+
+Print a string representation of the given quaternion `q`.
+"""
+Base.show(io::IO, q::Quaternion) = print(io, "($(q.a) + $(q.b) i + $(q.c) j + $(q.d) k) ∈ ℍ")
+
+
+"""
+    mat(q)
+
+Represent the number `q` by a complex 2x2 matrix in terms a basis of elementary Quaternions: I, i, j and k.
+"""
+mat(q::Quaternion) = [q.a + im * q.d -q.c + im * q.b; q.c + im * q.b q.a - im * q.d]
+
+
+"""
+    mat4(q)
+
+Represent the number `q` by a quaternionic 4x4 matrix in terms a basis for so(4), the Lie algebra of the Lie group of rotations about a fixed point in ℝ⁴.
+"""
+mat4(q::Quaternion) = q.a .* I(4) + q.b .* K(2) + q.c .* K(3) + q.d .* K(1)
+
+
+"""
+    real(q)
+
+Return the real part of the quaternion number `q`.
+"""
+Base.real(q::Quaternion) = q.a
+
+
+"""
+    imag(q)
+
+Return the imaginary (vectorial) part of the quaternion number `q`.
+"""
+Base.imag(q::Quaternion) = ℝ³(q.b, q.c, q.d)
+
+
+"""
+    conj(q)
+
+Compute the conjugate of a quaternion number `q`.
+"""
+Base.conj(q::Quaternion) = Quaternion(real(q), vec(-imag(q))...)
+
+
+"""
+    det(q)
+
+Compute the determinant of a quaternion number `q`.
+"""
+det(q::Quaternion) = det(mat(q))
+det(M::Matrix{<:Complex}) = begin
+    @assert(size(M) == (2, 2), "The size of the matrix must be a square 2 by 2, but was given the size: $(size(M)).")
+    real(M[1, 1] * M[2, 2] - M[1, 2] * M[2, 1])
+end
+
+
+"""
+    isapprox(g, q)
+
+Inexact equality comparison.
+"""
+Base.isapprox(g::Quaternion, q::Quaternion; atol::Float64 = TOLERANCE) = isapprox(g.a, q.a, atol = atol) &&
+                                                                         isapprox(g.b, q.b, atol = atol) &&
+                                                                         isapprox(g.c, q.c, atol = atol) &&
+                                                                         isapprox(g.d, q.d, atol = atol)
+
+
+"""
+    norm(q)
+
+Compute the 2-norm as if `q` were a vector of the corresponding length.
+"""
+norm(q::Quaternion) = norm(ℝ⁴(vec(q)))
+
+
+"""
+    normalize(q)
+
+Normalize the number `q` so that its 2-norm equals unity, i.e. norm(a) == 1.
+"""
+normalize(q::Quaternion) = Quaternion(normalize(ℝ⁴(vec(q))))
+
+
+"""
+    dot(g, q)
+
+Compute the dot product between two vector representations of `g` and `q`.
+"""
+dot(g::Quaternion, q::Quaternion) = dot(ℝ⁴(vec(g)), ℝ⁴(vec(q)))
+dot(g::ℝ⁴, q::Quaternion) = dot(g, ℝ⁴(vec(q)))
+dot(g::Quaternion, q::ℝ⁴) = dot(ℝ⁴(vec(g)), q)
+
+
++(q::Quaternion) = q
+-(q::Quaternion) = Quaternion(-vec(q))
+(+)(g::Quaternion, q::Quaternion) = Quaternion(mat4(g) + mat4(q))
+(-)(g::Quaternion, q::Quaternion) = Quaternion(mat4(g) - mat4(q))
+(*)(q::Quaternion, λ::Real) = Quaternion(mat4(q) .* λ)
+(*)(λ::Real, q::Quaternion) = q * λ
+(*)(g::Quaternion, q::Quaternion) = Quaternion(mat4(g) * mat4(q))
+(*)(m::Matrix{<:Real}, q::Quaternion) = Quaternion(m * vec(q))
+(*)(m::Matrix{<:Complex}, q::Quaternion) = m * mat(q)
 
 
 """
@@ -115,96 +262,3 @@ R = [1 0 0 0;
      0 -1 0 0;
      0 0 -1 0;
      0 0 0 -1]
-
-
-"""
-    vec(q)
- 
- Reshape the number `q` as a one-dimensional column vector in ℝ⁴. The resulting vector shares the same underlying data as `q`, so it will only be mutable if
- `q` is mutable, in which case modifying one will also modify the other.
- """
- Base.vec(q::Quaternion) = vec(q.a)
-
-
-"""
-    show(q)
-
-Print a string representation of the given quaternion `q`.
-"""
-Base.show(io::IO, q::Quaternion) = print(io, "($(vec(q)[1]) + $(vec(q)[2]) i + $(vec(q)[3]) j + $(vec(q)[4]) k) ∈ ℍ")
-
-
-"""
-    mat(q)
-
-Represent the number `q` by a quaternionic 4x4 matrix in terms a basis for so(4), the Lie algebra of the Lie group of rotations about a fixed point in ℝ⁴.
-"""
-mat(q::Quaternion) = vec(q)[1] .* I(4) + vec(q)[2] .* K(2) + vec(q)[3] .* K(3) + vec(q)[4] .* K(1)
-
-
-"""
-    real(q)
-
-Return the real part of the quaternion number `q`.
-"""
-Base.real(q::Quaternion) = vec(q)[1]
-
-
-"""
-    imag(q)
-
-Return the imaginary part of the quaternion number `q`.
-"""
-Base.imag(q::Quaternion) = ℝ³(vec(q)[2:4])
-
-
-"""
-    conj(q)
-
-Compute the conjugate of a quaternion number `q`.
-"""
-Base.conj(q::Quaternion) = Quaternion(transpose(R) * mat(q) * R)
-
-
-"""
-    isapprox(g, q)
-
-Inexact equality comparison.
-"""
-Base.isapprox(g::Quaternion, q::Quaternion; atol::Float64 = TOLERANCE) = isapprox(g.a, q.a, atol = atol)
-
-
-"""
-    norm(q)
-
-Compute the 2-norm as if `q` were a vector of the corresponding length.
-"""
-norm(q::Quaternion) = norm(q.a)
-
-
-"""
-    normalize(q)
-
-Normalize the number `q` so that its 2-norm equals unity, i.e. norm(a) == 1.
-"""
-normalize(q::Quaternion) = Quaternion(normalize(q.a))
-
-
-"""
-    dot(g, q)
-
-Compute the dot product between two vector representations of `g` and `q`.
-"""
-dot(g::Quaternion, q::Quaternion) = dot(g.a, q.a)
-dot(g::ℝ⁴, q::Quaternion) = dot(g, q.a)
-dot(g::Quaternion, q::ℝ⁴) = dot(g.a, q)
-
-
-+(q::Quaternion) = q
--(q::Quaternion) = Quaternion(-q.a)
-(+)(g::Quaternion, q::Quaternion) = Quaternion(mat(g) + mat(q))
-(-)(g::Quaternion, q::Quaternion) = Quaternion(mat(g) - mat(q))
-(*)(q::Quaternion, λ::Real) = Quaternion(mat(q) .* λ)
-(*)(λ::Real, q::Quaternion) = q * λ
-(*)(g::Quaternion, q::Quaternion) = Quaternion(mat(g) * mat(q))
-(*)(m::Matrix{<:Real}, q::Quaternion) = Quaternion(m * vec(q))
--- test/quaternions_tests.jl
+++ test/quaternions_tests.jl
@@ -1,3 +1,60 @@
+# the multiplication table
+table = [elI eli elj elk;
+         eli -elI elk -elj;
+         elj -elk -elI eli;
+         elk elj -eli -elI]
+for i in 1:4
+    for j in 1:4
+        basis1 = table[1:2, 2i - 1:2i]
+        basis2 = table[1:2, 2j - 1:2j]
+        @test isapprox(basis1 * basis2, table[2i - 1:2i, 2j - 1:2j])
+    end
+end
+
+
+## The space of abstract inner products
+
+u = Quaternion(rand(4))
+v = Quaternion(rand(4))
+v₁ = Quaternion(rand(4))
+v₂ = Quaternion(rand(4))
+z = Quaternion(0.0, 0.0, 0.0, 0.0)
+α, β = rand(2)
+@test isapprox(dot(u, v), dot(v, u)) # Symmetric
+@test isapprox(dot(u, α * v₁ + β * v₂), α * dot(u, v₁) + β * dot(u, v₂)) # Linear
+@test dot(u, u) ≥ 0 # Positive semidefinite I
+@test isapprox(dot(z, z), 0) # positive semidefinite II
+
+
+q = Quaternion(rand(4))
+@test isapprox(conj(conj(q)), q)
+@test isapprox(mat(conj(q)), elI .* q.a - eli .* q.b - elj .* q.c - elk .* q.d)
+@test isapprox(det(q), q.a^2 + q.b^2 + q.c^2 + q.d^2)
+h = q * conj(q)
+@test isapprox(mat(h), elI .* (q.a^2 + q.b^2 + q.c^2 + q.d^2))
+
+q = Quaternion(normalize(ℝ⁴(rand(4))))
+@test isapprox(norm(q), 1.0)
+@test isapprox(norm(q), q.a^2 + q.b^2 + q.c^2 + q.d^2)
+
+
+g = Quaternion(rand(4))
+q = Quaternion(rand(4))
+a = real(g)
+v = imag(g)
+a′ = real(q)
+v′ = imag(q)
+@test isapprox(g + q, Quaternion(a + a′, vec(v + v′)...))
+@test isapprox(g * q, Quaternion(a * a′ - dot(v, v′), vec(a′ * v + a * v′ + cross(v, v′))...))
+
+
+ψ = 2π
+u = normalize(ℝ³(rand(3)))
+g = Quaternion(0.0, u)
+q = Quaternion(ψ, u)
+@test isapprox(g, -q) # ψ ↦ ψ + 2π
+
+
 e = Quaternion(ℝ⁴(1.0, 0.0, 0.0, 0.0))
 i = Quaternion(0.0, 1.0, 0.0, 0.0)
 j = Quaternion(0.0, 0.0, 1.0, 0.0)
@@ -18,7 +75,8 @@ k = Quaternion(0.0, 0.0, 0.0, 1.0)
 u = normalize(ℝ³(rand(3)))
 q = Quaternion(θ, u)
 
-@test isapprox(conj(conj(q)), q)
+conjugate = Quaternion(transpose(R) * mat4(q) * R)
+@test isapprox(conj(q), conjugate)
 
 # |q| = 1
 @test isapprox(norm(q), 1)
@@ -31,8 +89,10 @@ q = Quaternion(θ, u)
 @test isapprox(K(3), transpose(R) * J(2) * R, atol = TOLERANCE)
 
 # Check to see if the following vectors form a basis for ℝ⁴
-ξ = Quaternion(rand(4)...)
+ξ = Quaternion(rand(4))
 
+# <ξ, ξ> = 1
+@test isapprox(norm(ξ)^2, abs(dot(ξ, ξ)), atol = TOLERANCE)
 # <ξ, K₁ξ> = 0
 @test isapprox(0, dot(ξ, K(1) * ξ), atol = TOLERANCE)
 # <ξ, K₂ξ> = 0
