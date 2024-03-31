1d9b7ac67982e828f6cdf3b641ca3a53a664ba48
Documenter.jl <documenter@juliadocs.github.io>
2024-03-31 21:30:41 +0000

    build based on cd08046

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"index.html","page":"Home","title":"Home","text":"Author = \"Mohammad Javad Azadi\"","category":"page"},{"location":"index.html#Welcome-to-the-documentation!","page":"Home","title":"Welcome to the documentation!","text":"","category":"section"}]
+}
