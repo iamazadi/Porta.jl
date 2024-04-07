c44902a15e95398984939031de20fcaf38c8c805
Mohammad Javad Azadi <mjazadi42@gmail.com>
2024-04-07 16:09:00 +0330

    Added an explicit guage transformation for obeying multiplication (on the right) rules.

--- src/basemap.jl
+++ src/basemap.jl
@@ -7,21 +7,14 @@ export make
 
 
 """
-    make(q, M, segments, chart)
+    make(q, gauge, M, segments, chart)
 
 Make a 2-surface of the horizontal section at point `q` after transformation with `f`, and with the given `segments` number and `chart`.
 """
-function make(q::Quaternion, f::Any, segments::Integer; chart::NTuple{4, Float64} = (-π / 4, π / 4, -π / 4, π / 4))
-    matrix = Matrix{ℝ³}(undef, segments, segments)
-    lspaceθ = collect(range(chart[1], stop = chart[2], length = segments))
-    lspaceϕ = collect(range(chart[3], stop = chart[4], length = segments))
-    for (i, ϕ) in enumerate(lspaceϕ)
-        for (j, θ) in enumerate(lspaceθ)
-            point = exp(θ * K(1) + -ϕ * K(2)) * q
-            matrix[i, j] = project(f(point))
-        end
-    end
-    matrix
+function make(q::Quaternion, gauge::Float64, f::Matrix{Float64}, segments::Integer; chart::NTuple{4, Float64} = (-π / 4, π / 4, -π / 4, π / 4))
+    lspaceθ = range(chart[1], stop = chart[2], length = segments)
+    lspaceϕ = range(chart[3], stop = chart[4], length = segments)
+    [project(normalize(f * (q * Quaternion(exp(θ * K(1) + -ϕ * K(2)) * exp(gauge * K(3)))))) for ϕ in lspaceϕ, θ in lspaceθ]
 end
 
 
@@ -32,28 +25,30 @@ fields: q, f, chart, segments, color and observable.
 """
 mutable struct Basemap <: Sprite
     q::Quaternion
-    f::Any
+    gauge::Float64
+    f::Matrix{Float64}
     chart::NTuple{4, Float64}
     segments::Integer
     color::Any
     observable::Tuple{GLMakie.Observable{Matrix{Float64}}, GLMakie.Observable{Matrix{Float64}}, GLMakie.Observable{Matrix{Float64}}}
-    Basemap(scene::GLMakie.LScene, q::Quaternion, f::Any, chart::NTuple{4, Float64}, segments::Integer, color::Any; transparency::Bool = false) = begin
-        matrix = make(q, f, segments, chart = chart)
+    Basemap(scene::GLMakie.LScene, q::Quaternion, gauge::Float64, f::Matrix{Float64}, chart::NTuple{4, Float64}, segments::Integer, color::Any; transparency::Bool = false) = begin
+        matrix = make(q, gauge, f, segments, chart = chart)
         observable = buildsurface(scene, matrix, color, transparency = transparency)
-        new(q, f, chart, segments, color, observable)
+        new(q, gauge, f, chart, segments, color, observable)
     end
 end
 
 
 """
-    update!(basemap, q, M)
+    update!(basemap, q, gauge, M)
 
-Switch to the right horizontal section with the given point `q` and transformation `f`.
+Switch to the right horizontal section with the given point `q`, `gauge` and transformation `f`.
 """
-function update!(basemap::Basemap, q::Quaternion, f::Any)
+function update!(basemap::Basemap, q::Quaternion, gauge::Float64, f::Matrix{Float64})
     basemap.q = q
+    basemap.gauge = gauge
     basemap.f = f
-    matrix = make(q, f, basemap.segments, chart = basemap.chart)
+    matrix = make(q, gauge, f, basemap.segments, chart = basemap.chart)
     updatesurface!(matrix, basemap.observable)
 end
 
@@ -65,7 +60,7 @@ Update the bundle chart in the horizontal subspace with the given 'chart.
 """
 function update!(basemap::Basemap, chart::NTuple{4, Float64})
     basemap.chart = chart
-    matrix = make(basemap.q, basemap.f, basemap.segments, chart = chart)
+    matrix = make(basemap.q, basemap.gauge, basemap.f, basemap.segments, chart = chart)
     updatesurface!(matrix, basemap.observable)
 end
 
--- test/basemap_tests.jl
+++ test/basemap_tests.jl
@@ -2,21 +2,26 @@ import GLMakie
 import FileIO
 
 
+q = Quaternion(normalize(ℝ⁴(rand(4))))
+gauge = rand() * 2π
+f = I(4)
+chart = (-π / 4, π / 4, -π / 4, π / 4)
+matrix = make(q, gauge, f, segments, chart = chart)
+@test size(matrix) == (segments, segments)
+@test typeof(matrix[1, 1]) <: ℝ³
+
+
 fig = GLMakie.Figure()
 lscene = GLMakie.LScene(fig[1, 1])
 
 segments = rand(5:10)
 color = FileIO.load("../data/basemap_color.png")
 transparency = rand(1:2) == 1 ? true : false
-q = Quaternion(normalize(ℝ⁴(rand(4))))
-M = rand(4, 4)
-f(x::Quaternion) = normalize(M * x)
-chart = (-π / 4, π / 4, -π / 4, π / 4)
-basemap = Basemap(lscene, q, f, chart, segments, color, transparency = transparency)
+basemap = Basemap(lscene, q, gauge, f, chart, segments, color, transparency = transparency)
 
 matrix = getsurface(basemap.observable, segments, segments)
 q = Quaternion(normalize(ℝ⁴(rand(4))))
-update!(basemap, q, f)
+update!(basemap, q, gauge, f)
 _matrix = getsurface(basemap.observable, segments, segments)
 
 @test all([!isapprox(matrix[i, j], _matrix[i, j]) for i in 1:segments for j in 1:segments])
