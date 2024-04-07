4c5e0abef6d5acefa4f4854ab258ae613610c24e
Mohammad Javad Azadi <mjazadi42@gmail.com>
2024-04-07 16:10:07 +0330

    Specified the transformation type for performance.

--- src/whirl.jl
+++ src/whirl.jl
@@ -17,16 +17,9 @@ abstract type Sprite end
 
 Make the vertical subspace of the boundary `q`, with the given heights `θ1` and `θ2`, transform `f` and the number of `segments`.
 """
-function make(q::Vector{Quaternion}, θ1::Float64, θ2::Float64, f::Any, segments::Integer)
-    matrix = Matrix{ℝ³}(undef, length(q), segments)
-    lspaceθ = collect(range(θ1, stop = θ2, length = segments))
-    for (j, p) in enumerate(q)
-        for (i, θ) in enumerate(lspaceθ)
-            point = exp(K(3) * θ) * p
-            matrix[j, i] = project(f(point))
-        end
-    end
-    matrix
+function make(q::Vector{Quaternion}, θ1::Float64, θ2::Float64, f::Matrix{Float64}, segments::Integer)
+    lspaceθ = range(θ1, stop = θ2, length = segments)
+    [project(normalize(f * (q[i] * Quaternion(exp(K(3) * θ))))) for i in 1:length(q), θ in lspaceθ]
 end
 
 
@@ -39,11 +32,11 @@ mutable struct Whirl <: Sprite
     q::Vector{Quaternion}
     θ1::Float64
     θ2::Float64
-    f::Any
+    f::Matrix{Float64}
     segments::Integer
     color::Any
     observable::Tuple{GLMakie.Observable{Matrix{Float64}}, GLMakie.Observable{Matrix{Float64}}, GLMakie.Observable{Matrix{Float64}}}
-    Whirl(scene::GLMakie.LScene, q::Vector{Quaternion}, θ1::Float64, θ2::Float64, f::Any, segments::Integer, color::Any; transparency::Bool = false) = begin
+    Whirl(scene::GLMakie.LScene, q::Vector{Quaternion}, θ1::Float64, θ2::Float64, f::Matrix{Float64}, segments::Integer, color::Any; transparency::Bool = false) = begin
         matrix = make(q, θ1, θ2, f, segments)
         color_matrix = GLMakie.Observable(fill(color, length(q), segments))
         observable = buildsurface(scene, matrix, color_matrix, transparency = transparency)
@@ -57,7 +50,7 @@ end
 
 Update the shape of a `whirl` with the given boundary points `q`, gauges `θ1` and `θ2`, and transformation `f`.
 """
-function update!(whirl::Whirl, q::Vector{Quaternion}, θ1::Float64, θ2::Float64, f::Any)
+function update!(whirl::Whirl, q::Vector{Quaternion}, θ1::Float64, θ2::Float64, f::Matrix{Float64})
     whirl.q = q
     whirl.θ1 = θ1
     whirl.θ2 = θ2
--- test/whirl_tests.jl
+++ test/whirl_tests.jl
@@ -6,22 +6,21 @@ q = [Quaternion(normalize(ℝ⁴(rand(4)))) for i in 1:N]
 θ1 = rand()
 θ2 = rand()
 segments = rand(5:10)
-M = rand(4, 4)
-whirlf(x::Quaternion) = normalize(M * x)
-matrix = make(q, θ1, θ2, whirlf, segments)
+f = I(4)
+matrix = make(q, θ1, θ2, f, segments)
 @test size(matrix) == (N, segments)
 @test typeof(matrix[1, 1]) <: ℝ³
 
 fig = GLMakie.Figure()
 lscene = GLMakie.LScene(fig[1, 1])
 color = GLMakie.RGBAf(rand(4)...)
-whirl = Whirl(lscene, q, θ1, θ2, whirlf, segments, color, transparency = false)
+whirl = Whirl(lscene, q, θ1, θ2, f, segments, color, transparency = false)
 
 _q = [Quaternion(normalize(ℝ⁴(rand(4)))) for i in 1:N]
 _θ1 = rand()
 _θ2 = rand()
 
-update!(whirl, _q, _θ1, _θ2, whirlf)
+update!(whirl, _q, _θ1, _θ2, f)
 
 @test all([isapprox(whirl.q[i], _q[i]) for i in 1:N])
 @test all([isapprox(whirl.θ1, _θ1) for i in 1:N])
