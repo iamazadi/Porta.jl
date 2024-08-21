188c4cf12abb8e13721b3efd58cd51070c57d25a
Mohammad Javad Azadi <mjazadi42@gmail.com>
2024-08-21 03:23:38 -0700

    Measured the rotation and position matrices of sensors.

--- models/unicycle.jl
+++ models/unicycle.jl
@@ -4,8 +4,98 @@ import Makie
 import MeshIO
 import GeometryBasics
 import LinearAlgebra
+import GLMakie.Quaternion
 using Sockets
 using Porta
+
+
+GLMakie.Quaternion(q::Porta.Quaternion) = GLMakie.Quaternion(vec(q)[2:4]..., vec(q)[1])
+
+
+parse3vector(text::String, beginninglabel::String, endinglabel::String, delimiter::String) = begin
+    vector = []
+    index = findfirst(beginninglabel, text)
+    if !isnothing(index)
+        _text = replace(text, text[begin:index[end]] => "")
+        if length(_text) > 3
+            index = findfirst(endinglabel, _text)
+            if !isnothing(index)
+                _text = replace(_text, _text[index[begin]:end] => "")
+                if length(_text) > 3
+                    _text = strip(_text)
+                    items = split(_text, delimiter)
+                    try
+                        push!(vector, parse(Float64, items[1]))
+                        push!(vector, parse(Float64, items[2]))
+                        push!(vector, parse(Float64, items[3]))
+                    catch e
+                        println("Not parsed: $_text. $e")
+                    end
+                end
+            end
+        end
+    end
+    vector
+end
+
+
+parsescalar(text::String, beginninglabel::String, endinglabel::String) = begin
+    scalar = Nothing
+    index = findfirst(beginninglabel, text)
+    if !isnothing(index)
+        _text = replace(text, text[begin:index[end]] => "")
+        if length(_text) > 1
+            index = findfirst(endinglabel, _text)
+            if !isnothing(index)
+                _text = replace(_text, _text[index[begin]:end] => "")
+                if length(_text) > 0
+                    _text = strip(_text)
+                    try
+                        scalar = parse(Int, _text)
+                    catch e
+                        println("Not parsed: $_text. $e")
+                    end
+                end
+            end
+        end
+    end
+    scalar
+end
+
+
+parsetext(text::String) = begin
+    readings = Dict()
+    if length(text) > 30
+        vector = parse3vector(text, "A1: ", "A2: ", ",")
+        if length(vector) == 3
+            readings["R1"] = vector
+        end
+        vector = parse3vector(text, "A2: ", "A3: ", ",")
+        if length(vector) == 3
+            readings["R2"] = vector
+        end
+        vector = parse3vector(text, "A3: ", "A4: ", ",")
+        if length(vector) == 3
+            readings["R3"] = vector
+        end
+        vector = parse3vector(text, "A4: ", "c1: ", ",")
+        if length(vector) == 3
+            readings["R4"] = vector
+        end
+        scalar = parsescalar(text, "c1: ", ", ")
+        if !isnothing(scalar)
+            readings["v1"] = scalar
+        end
+        scalar = parsescalar(text, "c2: ", ", ")
+        if !isnothing(scalar)
+            readings["v2"] = scalar
+        end
+    end
+    readings
+end
+
+
+rotate(point::Vector{Float64}, q::Porta.Quaternion) = vec(conj(q) * Porta.Quaternion(0.0, (point)...) * q)[2:4]
 
 
 find_centerofmass(stl) = begin
@@ -64,21 +154,22 @@ chassis_origin = GLMakie.Point3f(-0.1, -0.1, -0.02)
 rollingwheel_origin = GLMakie.Point3f(3.0, -12.0, 0.0)
 reactionwheel_origin = GLMakie.Point3f(0.0, 153.0, 1.0)
 
-chassis_qx = Quaternion(0.0, x̂)
-chassis_qy = Quaternion(0.0, ŷ)
-chassis_qz = Quaternion(pi / 2.0, ẑ)
+chassis_qx = Porta.Quaternion(π / 2, x̂)
+chassis_qy = Porta.Quaternion(0.0, ŷ)
+# chassis_qz = Porta.Quaternion(pi / 2.0, ẑ)
+chassis_qz = Porta.Quaternion(0.0, ẑ)
 chassis_q0 = chassis_qx * chassis_qy * chassis_qz
-chassis_rotation = GLMakie.Quaternion(vec(chassis_q0)...)
-rollingwheel_qx = Quaternion(0.0, x̂)
-rollingwheel_qy = Quaternion(0.0, ŷ)
-rollingwheel_qz = Quaternion(0.0, ẑ) # the axis of rotation
+chassis_rotation = GLMakie.Quaternion(chassis_q0)
+rollingwheel_qx = Porta.Quaternion(0.0, x̂)
+rollingwheel_qy = Porta.Quaternion(0.0, ŷ)
+rollingwheel_qz = Porta.Quaternion(0.0, ẑ) # the axis of rotation
 rollingwheel_q0 = rollingwheel_qx * rollingwheel_qy * rollingwheel_qz
-rollingwheel_rotation = GLMakie.Quaternion(vec(rollingwheel_q0)...)
-reactionwheel_qx = Quaternion(0.0, x̂)
-reactionwheel_qy = Quaternion(0.0, ŷ)
-reactionwheel_qz = Quaternion(0.0, ẑ) # the axis of rotation
+rollingwheel_rotation = GLMakie.Quaternion(rollingwheel_q0)
+reactionwheel_qx = Porta.Quaternion(0.0, x̂)
+reactionwheel_qy = Porta.Quaternion(0.0, ŷ)
+reactionwheel_qz = Porta.Quaternion(0.0, ẑ) # the axis of rotation
 reactionwheel_q0 = reactionwheel_qx * reactionwheel_qy * reactionwheel_qz
-reactionwheel_rotation = GLMakie.Quaternion(vec(reactionwheel_q0)...)
+reactionwheel_rotation = GLMakie.Quaternion(reactionwheel_q0)
 
 chassis_stl_path = joinpath("data", "unicycle", "unicycle_chassis.STL")
 rollingwheel_stl_path = joinpath("data", "unicycle", "unicycle_main_wheel.STL")
@@ -124,11 +215,14 @@ GLMakie.arrows!(lscene,
     align = :center
 )
 
-point1 = GLMakie.Observable(GLMakie.Point3f(-0.05, -0.135, -0.03))
+point1 = GLMakie.Observable(GLMakie.Point3f(-0.035, -0.19, -0.04))
 point2 = GLMakie.Observable(GLMakie.Point3f(0.025, -0.144, -0.07))
 point3 = GLMakie.Observable(GLMakie.Point3f(-0.11, -0.01, 0.13))
 point4 = GLMakie.Observable(GLMakie.Point3f(-0.11, -0.19, 0.13))
-# ball = GLMakie.meshscatter!(lscene, point3, markersize = 0.01, color = :red) # for measurements of the configuration space
+ball1 = GLMakie.meshscatter!(lscene, point1, markersize = 0.01, color = :red) # for measurements of the configuration space
+ball2 = GLMakie.meshscatter!(lscene, point2, markersize = 0.01, color = :green)
+ball3 = GLMakie.meshscatter!(lscene, point3, markersize = 0.01, color = :blue)
+ball4 = GLMakie.meshscatter!(lscene, point4, markersize = 0.01, color = :orange)
 reference_point1 = deepcopy(point1[])
 reference_point2 = deepcopy(point2[])
 reference_point3 = deepcopy(point3[])
@@ -139,159 +233,76 @@ lookat = deepcopy(_p)
 GLMakie.update_cam!(lscene.scene, GLMakie.Vec3f(eyeposition...), GLMakie.Vec3f(lookat...), GLMakie.Vec3f(up...))
 
 
-parse3vector(text::String, beginninglabel::String, endinglabel::String, delimiter::String) = begin
-    vector = []
-    index = findfirst(beginninglabel, text)
-    if !isnothing(index)
-        _text = replace(text, text[begin:index[end]] => "")
-        if length(_text) > 3
-            index = findfirst(endinglabel, _text)
-            if !isnothing(index)
-                _text = replace(_text, _text[index[begin]:end] => "")
-                if length(_text) > 3
-                    _text = strip(_text)
-                    println(_text)
-                    items = split(_text, delimiter)
-                    try
-                        push!(vector, parse(Float64, items[1]))
-                        push!(vector, parse(Float64, items[2]))
-                        push!(vector, parse(Float64, items[3]))
-                    catch e
-                        println("Not parsed: $_text. $e")
-                    end
-                end
-            end
-        end
-    end
-    vector
-end
-
-
-parsescalar(text::String, beginninglabel::String, endinglabel::String) = begin
-    scalar = Nothing
-    index = findfirst(beginninglabel, text)
-    if !isnothing(index)
-        _text = replace(text, text[begin:index[end]] => "")
-        if length(_text) > 1
-            index = findfirst(endinglabel, _text)
-            if !isnothing(index)
-                _text = replace(_text, _text[index[begin]:end] => "")
-                if length(_text) > 0
-                    _text = strip(_text)
-                    println(_text)
-                    try
-                        scalar = parse(Int, _text)
-                    catch e
-                        println("Not parsed: $_text. $e")
-                    end
-                end
-            end
-        end
-    end
-    scalar
-end
-
-
-parsetext(text::String) = begin
-    readings = Dict()
-    if length(text) > 30
-        vector = parse3vector(text, "A1: ", "A2: ", ",")
-        if length(vector) == 3
-            readings["R1"] = vector
-        end
-        vector = parse3vector(text, "A2: ", "A3: ", ",")
-        if length(vector) == 3
-            readings["R2"] = vector
-        end
-        vector = parse3vector(text, "A3: ", "A4: ", ",")
-        if length(vector) == 3
-            readings["R3"] = vector
-        end
-        vector = parse3vector(text, "A4: ", "c1: ", ",")
-        if length(vector) == 3
-            readings["R4"] = vector
-        end
-        scalar = parsescalar(text, "c1: ", ", ")
-        if !isnothing(scalar)
-            readings["v1"] = scalar
-        end
-        scalar = parsescalar(text, "c2: ", ", ")
-        if !isnothing(scalar)
-            readings["v2"] = scalar
-        end
-    end
-    readings
-end
-
-
-rotate(point::Vector{Float64}, q::Quaternion) = vec(conj(q) * Quaternion(0.0, (point)...) * q)[2:4]
-
-
 clientside  = connect("192.168.4.1", 10000)
 
 run = true
-counter = 0
 errormonitor(@async while (isopen(clientside) && run)
+    # sleep(0.01)
     text = readline(clientside, keep = true)
-    global counter += 1
-    println(text)
+    # println(text)
     readings = parsetext(text)
     flag = "R1" in keys(readings) && "R2" in keys(readings) && "R3" in keys(readings) && "R4" in keys(readings) && "v1" in keys(readings) && "v2" in keys(readings)
-    if flag && counter > 10
-        global counter = 0
+    if flag
         R1 = readings["R1"]
         R2 = readings["R2"]
         R3 = readings["R3"]
         R4 = readings["R4"]
         v1 = readings["v1"]
         v2 = readings["v2"]
-        R1 = R1
-        R2 = R2
-        R3 = R3
-        R4 = R4
-        R0 = GLMakie.Vec3f(R1[1], R1[2], R1[3]) + GLMakie.Vec3f(-R2[1], R2[2], -R2[3]) + GLMakie.Vec3f(R3[3], R3[1], R3[2]) + GLMakie.Vec3f(R4[3], -R4[1], -R4[2])
-        R0 = R0 .* 0.25
-        pitch = atan(R0[1], R0[3])
-        roll = atan(R0[2], R0[3])
+        # R1 = [-R1[1]; R1[2]; R1[3]]
+        # R2 = [R2[1]; R2[2]; -R2[3]]
+        # R3 = [-R3[3]; R3[1]; R3[2]]
+        # R4 = [-R4[3]; -R4[1]; -R4[2]]
+        R1 = [R1[2]; R1[1]; R1[3]]
+        R2 = [-R2[1]; -R2[2]; -R2[3]]
+        R3 = [-R3[1]; -R3[2]; -R3[3]]
+        R4 = [-R4[1]; -R4[2]; -R4[3]]
+        R0 = (R1 + R2 + R3 + R4) .* 0.25
+        pitch = atan(R1[1], R1[3])
+        roll = atan(R1[2], R1[3])
         yaw = 0.0
-        global R1 = R1 .* arrow_scale
-        global R2 = R2 .* arrow_scale
-        global R3 = R3 .* arrow_scale
-        global R4 = R4 .* arrow_scale
+        println("Pitch: $pitch, Roll: $roll")
+        # cr = cos(pitch * 0.5) # cosine roll
+        # sr = sin(pitch * 0.5) # sine roll
+        # cp = cos(yaw * 0.5) # cos pitch
+        # sp = sin(yaw * 0.5) # sine pitch
+        # cy = cos(roll * 0.5) # cos yaw
+        # sy = sin(roll * 0.5) # sine yaw
 
-        cr = cos(pitch * 0.5) # cosine roll
-        sr = sin(pitch * 0.5) # sine roll
-        cp = cos(yaw * 0.5) # cos pitch
-        sp = sin(yaw * 0.5) # sine pitch
-        cy = cos(roll * 0.5) # cos yaw
-        sy = sin(roll * 0.5) # sine yaw
+        # cr = cos(roll * 0.5) # cosine roll
+        # sr = sin(roll * 0.5) # sine roll
+        # cp = cos(pitch * 0.5) # cos pitch
+        # sp = sin(pitch * 0.5) # sine pitch
+        # cy = cos(yaw * 0.5) # cos yaw
+        # sy = sin(yaw * 0.5) # sine yaw
 
-        w = cr * cp * cy + sr * sp * sy
-        x = sr * cp * cy - cr * sp * sy
-        y = cr * sp * cy + sr * cp * sy
-        z = cr * cp * sy - sr * sp * cy
-        q = Quaternion(w, x, y, z)
-        chassis_q = q * chassis_q0
-        q = GLMakie.Quaternion(vec(chassis_q)...)
-        GLMakie.rotate!(robot, q)
-
+        # w = cr * cp * cy + sr * sp * sy
+        # x = sr * cp * cy - cr * sp * sy
+        # y = cr * sp * cy + sr * cp * sy
+        # z = cr * cp * sy - sr * sp * cy
         
-        mq = GLMakie.Quaternion(vec(Quaternion(float(v1) / 600.0 * 2pi, x̂))...)
-        rq = GLMakie.Quaternion(vec(Quaternion(float(-v2) / 1800.0 * 2pi, ẑ))...)
-        GLMakie.rotate!(rollingwheel, mq)
-        GLMakie.rotate!(reactionwheel, rq)
-
-        g = Quaternion(roll, x̂) * Quaternion(-pitch, ŷ)
+        # q = Porta.Quaternion(w, x, y, z)
+        # ang, u = getrotation(ẑ, ℝ³(R0...))
+        # q = Porta.Quaternion(ang, u)
+        q = Porta.Quaternion(roll, x̂) * Porta.Quaternion(pitch, ŷ)
+        g = q * chassis_q0
+        GLMakie.rotate!(robot, GLMakie.Quaternion(g)) # change the comibination to correct for an API difference
+        # g = Porta.Quaternion(roll, x̂) * Porta.Quaternion(pitch, ŷ)
         p1 = deepcopy(Float64.([reference_point1...]))
         p2 = deepcopy(Float64.([reference_point2...]))
         p3 = deepcopy(Float64.([reference_point3...]))
         p4 = deepcopy(Float64.([reference_point4...]))
-        point1[] = GLMakie.Point3f(rotate(p1 - _p, g) + _p)
-        point2[] = GLMakie.Point3f(rotate(p2 - _p, g) + _p)
-        point3[] = GLMakie.Point3f(rotate(p3 - _p, g) + _p)
-        point4[] = GLMakie.Point3f(rotate(p4 - _p, g) + _p)
+        point1[] = GLMakie.Point3f(rotate(p1 - _p, q) + _p)
+        point2[] = GLMakie.Point3f(rotate(p2 - _p, q) + _p)
+        point3[] = GLMakie.Point3f(rotate(p3 - _p, q) + _p)
+        point4[] = GLMakie.Point3f(rotate(p4 - _p, q) + _p)
         ps[] = [GLMakie.Point3f(point1[]...), GLMakie.Point3f(point2[]...), GLMakie.Point3f(point3[]...), GLMakie.Point3f(point4[]...)]
-        ns[] = [GLMakie.Vec3f(-R1[1], R1[2], R1[3]), GLMakie.Vec3f(R2[1], R2[2], -R2[3]), GLMakie.Vec3f(-R3[3], R3[1], R3[2]),  GLMakie.Vec3f(-R4[3], -R4[1], -R4[2])]
+        ns[] = map(x -> x .* arrow_scale, [GLMakie.Vec3f(R1...), GLMakie.Vec3f(R2...), GLMakie.Vec3f(R3...),  GLMakie.Vec3f(R4...)])
+
+        mq = GLMakie.Quaternion(Porta.Quaternion(float(-v1) / 600.0 * 2pi, ẑ))
+        rq = GLMakie.Quaternion(Porta.Quaternion(float(-v2) / 1800.0 * 2pi, x̂))
+        GLMakie.rotate!(rollingwheel, mq)
+        GLMakie.rotate!(reactionwheel, rq)
     end
 end)
 
