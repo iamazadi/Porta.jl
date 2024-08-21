2e5d82b89480d0e5ec3b3e5eb31a63cd0939cad7
Mohammad Javad Azadi <mjazadi42@gmail.com>
2024-08-21 03:26:46 -0700

    Aligned the sensor frames to be in the same frame for state estimation.

--- models/unicycle.jl
+++ models/unicycle.jl
@@ -253,13 +253,13 @@ errormonitor(@async while (isopen(clientside) && run)
         # R2 = [R2[1]; R2[2]; -R2[3]]
         # R3 = [-R3[3]; R3[1]; R3[2]]
         # R4 = [-R4[3]; -R4[1]; -R4[2]]
-        R1 = [R1[2]; R1[1]; R1[3]]
-        R2 = [-R2[1]; -R2[2]; -R2[3]]
-        R3 = [-R3[1]; -R3[2]; -R3[3]]
-        R4 = [-R4[1]; -R4[2]; -R4[3]]
+        R1 = [-R1[2]; R1[1]; R1[3]]
+        R2 = [R2[1]; -R2[2]; -R2[3]]
+        R3 = [R3[1]; -R3[2]; -R3[3]]
+        R4 = [R4[1]; -R4[2]; -R4[3]]
         R0 = (R1 + R2 + R3 + R4) .* 0.25
-        pitch = atan(R1[1], R1[3])
-        roll = atan(R1[2], R1[3])
+        pitch = atan(R0[1], R0[3])
+        roll = atan(R0[2], R0[3])
         yaw = 0.0
         println("Pitch: $pitch, Roll: $roll")
         # cr = cos(pitch * 0.5) # cosine roll
